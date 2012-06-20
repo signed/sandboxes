6948057dff259c99461b0f167631955db3caa7f1
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2012-06-20 20:19:01 +0200

    made the local maven repository known to grails

--- sandbox-grails/grails-1.3/version-1.3/grails-app/conf/BuildConfig.groovy
+++ sandbox-grails/grails-1.3/version-1.3/grails-app/conf/BuildConfig.groovy
@@ -19,6 +19,7 @@ grails.project.dependency.resolution = {
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
+        mavenLocal()
         grailsPlugins()
         grailsHome()
         grailsCentral()
