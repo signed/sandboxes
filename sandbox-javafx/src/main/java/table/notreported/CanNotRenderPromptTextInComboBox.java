1bbe303e13b78cb77aa852f192153f7e5d879974
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2012-06-26 13:28:12 +0200

    Sample for ComboBox

---
+++ sandbox-javafx/src/main/java/table/notreported/CanNotRenderPromptTextInComboBox.java
@@ -0,0 +1,41 @@
+package table.notreported;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+
+public class CanNotRenderPromptTextInComboBox extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        FlowPane pane = new FlowPane();
+        ComboBox<String> comboBox = new ComboBox<>();
+        comboBox.setPrefWidth(500);
+        pane.getChildren().add(comboBox);
+        comboBox.buttonCellProperty().set(new ListCell<String>() {
+            @Override
+            protected void updateItem(String item, boolean isEmpty) {
+                super.updateItem(item, isEmpty);
+                if (isEmpty) {
+                    setText("Prompt Text");
+                    return;
+                }
+                setText(item);
+            }
+        });
+
+        comboBox.setItems(FXCollections.observableArrayList("one", "two"));
+
+        stage.setScene(new Scene(pane));
+        stage.show();
+    }
+}
