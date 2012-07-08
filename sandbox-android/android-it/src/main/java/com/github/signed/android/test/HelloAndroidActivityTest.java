0117e22085aea9c5354bcbde5baf32c6f4c4f5d5
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2012-07-08 10:00:00 +0200

    added android sample to sandbox

---
+++ sandbox-android/android-it/src/main/java/com/github/signed/android/test/HelloAndroidActivityTest.java
@@ -0,0 +1,17 @@
+package com.github.signed.android.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.github.signed.android.*;
+
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+    public HelloAndroidActivityTest() {
+        super(HelloAndroidActivity.class); 
+    }
+
+    public void testActivity() {
+        HelloAndroidActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
+
