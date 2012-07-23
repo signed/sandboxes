a8ab92bf51b417b4685fee0f38ee289a82263eac
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2012-07-23 11:00:21 +0200

    Last Style file is persisted on close and loaded on start

--- sandbox-javafx/src/main/java/styling/CssFun.java
+++ sandbox-javafx/src/main/java/styling/CssFun.java
@@ -7,6 +7,7 @@ import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ListChangeListener;
+import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
@@ -14,7 +15,15 @@ import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class CssFun extends Application {
@@ -26,27 +35,63 @@ public class CssFun extends Application {
     private final StyleManager styleManger = StyleManager.getInstance();
     private final CSSParser parser = CSSParser.getInstance();
     private final Node styleable = new ToggleButton("Ich bin der Text");
+    private final TextArea styleInput = new TextArea();
     private final TextArea errorOutput = new TextArea();
+    private final Path styleFile = Paths.get("style.css");
 
 
     @Override
     public void start(Stage stage) throws Exception {
+        String style = loadStyleFromLastRun();
 
-
-        HBox workbench = createWorkbenchPane();
+        HBox workbench = createWorkbenchPane(style);
         BorderPane mainPane = new BorderPane();
         mainPane.setCenter(workbench);
         mainPane.setBottom(errorOutput);
-        stage.setScene(new Scene(mainPane));
+
+
+        Scene scene = new Scene(mainPane);
+
+        stage.setScene(scene);
+
+        stage.onCloseRequestProperty().set(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent windowEvent) {
+                String[] lines = styleInput.getText().split("\n");
+                ArrayList<String> strings = new ArrayList<String>();
+
+                Collections.addAll(strings, lines);
+                try {
+                    Files.write(styleFile, strings, Charset.forName("UTF-8") );
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
         stage.show();
     }
 
-    private HBox createWorkbenchPane() {
-        TextArea styleInput = new TextArea();
+    private String loadStyleFromLastRun() throws IOException {
+        String style;
+        if (Files.exists(styleFile)) {
+            List<String> strings = Files.readAllLines(styleFile, Charset.forName("UTF-8"));
+            StringBuilder all = new StringBuilder();
+            for (String string : strings) {
+                all.append(string).append("\n");
+            }
+            style = all.toString();
+        } else {
+            Files.createFile(styleFile);
+            style = "";
+        }
+        return style;
+    }
 
+    private HBox createWorkbenchPane(String style) {
         styleManger.errorsProperty().addListener(new ListChangeListener<CssError>() {
             @Override
             public void onChanged(Change<? extends CssError> change) {
+                errorOutput.clear();
                 while(change.next()){
                     List<? extends CssError> added = change.getAddedSubList();
                     for (CssError cssError : added) {
@@ -61,13 +106,12 @@ public class CssFun extends Application {
         styleInput.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observableValue, String s, String s1) {
-                //errorOutput.clear();
                 parser.parse(s1);
                 styleable.setStyle(s1);
             }
         });
 
-        styleInput.setText("-fx-base: lightgreen;");
+        styleInput.setText(style);
 
         BorderPane componentPane = new BorderPane();
         componentPane.setCenter(styleable);
