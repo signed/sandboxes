e1c0651be6e6106c67ea29635e6495c700251368
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2012-08-21 09:07:29 +0200

    Setup the scenario for Reproduction

--- sandbox-intellij/move-static-variables/application/src/main/java/some/other/ImportingClass.java
+++ sandbox-intellij/move-static-variables/application/src/main/java/some/other/ImportingClass.java
@@ -1,4 +1,12 @@
 package some.other;
 
+import apackage.TimeConstants;
+
+import static apackage.TimeConstants.Constant;
+
 public class ImportingClass {
+    public static void main(String[] args) {
+        System.out.println(Constant);
+        System.out.println(TimeConstants.Constant);
+    }
 }
--- sandbox-intellij/move-static-variables/dependency/main/java/apackage/TimeConstants.java
+++ sandbox-intellij/move-static-variables/dependency/main/java/apackage/TimeConstants.java
@@ -1,4 +1,6 @@
 package apackage;
 
 public class TimeConstants {
+
+    public static String Constant = "important";
 }
