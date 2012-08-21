d8d30792ba24048f405178b01d7e91829af9cb1a
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2012-08-21 09:13:46 +0200

    Setup simpler scenario

--- sandbox-intellij/move-static-variables/application/src/main/java/some/other/ImportingClass.java
+++ sandbox-intellij/move-static-variables/application/src/main/java/some/other/ImportingClass.java
@@ -1,12 +1,17 @@
 package some.other;
 
 import apackage.TimeConstants;
+import common.Constants;
 
 import static apackage.TimeConstants.Constant;
+import static common.Constants.ConstantFromSameModule;
 
 public class ImportingClass {
     public static void main(String[] args) {
         System.out.println(Constant);
         System.out.println(TimeConstants.Constant);
+
+        System.out.println(ConstantFromSameModule);
+        System.out.println(Constants.ConstantFromSameModule);
     }
 }
