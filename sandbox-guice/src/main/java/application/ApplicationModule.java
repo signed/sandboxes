b820abf2357c1d790e5c2f167f6ef950e0f618b1
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2012-08-28 20:34:26 +0200

    Binding for plugin is in its own module

--- sandbox-guice/src/main/java/application/ApplicationModule.java
+++ sandbox-guice/src/main/java/application/ApplicationModule.java
@@ -2,8 +2,6 @@ package application;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
-import com.google.inject.multibindings.Multibinder;
-import contribution.ContributionOne;
 import contribution.JavaFxOneView;
 import contribution.OneView;
 
@@ -12,8 +10,5 @@ public class ApplicationModule extends AbstractModule {
     protected void configure() {
         bind(OneView.class).to(JavaFxOneView.class);
         bind(ApplicationModel.class).in(Singleton.class);
-
-        Multibinder<ToolBarContribution> toolBarContributionMultibinder = Multibinder.newSetBinder(binder(), ToolBarContribution.class);
-        toolBarContributionMultibinder.addBinding().to(ContributionOne.class);
     }
 }
--- sandbox-guice/src/main/java/contribution/ContributionOneModule.java
+++ sandbox-guice/src/main/java/contribution/ContributionOneModule.java
@@ -1,10 +1,13 @@
 package contribution;
 
+import application.ToolBarContribution;
 import com.google.inject.AbstractModule;
+import com.google.inject.multibindings.Multibinder;
 
 public class ContributionOneModule extends AbstractModule{
     @Override
     protected void configure() {
-
+        Multibinder<ToolBarContribution> toolBarContributionMultibinder = Multibinder.newSetBinder(binder(), ToolBarContribution.class);
+        toolBarContributionMultibinder.addBinding().to(ContributionOne.class);
     }
 }
