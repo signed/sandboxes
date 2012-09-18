93244b855709bbeb5b5c4b1223132b4049f18abc
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2012-09-18 13:59:47 +0200

    Filter is applied

--- sandbox-maven/resource-plugin/custom-filter/src/main/java/com/github/signed/maven/ItFilter.java
+++ sandbox-maven/resource-plugin/custom-filter/src/main/java/com/github/signed/maven/ItFilter.java
@@ -1,21 +1,53 @@
 package com.github.signed.maven;
 
-import org.apache.maven.shared.filtering.DefaultMavenResourcesFiltering;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.model.Resource;
+import org.apache.maven.project.MavenProject;
 import org.apache.maven.shared.filtering.MavenFilteringException;
 import org.apache.maven.shared.filtering.MavenResourcesExecution;
+import org.apache.maven.shared.filtering.MavenResourcesFiltering;
+import org.codehaus.plexus.util.FileUtils;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @plexus.component role="org.apache.maven.shared.filtering.MavenResourcesFiltering"
  *                   role-hint="itFilter"
  */
-public class ItFilter extends DefaultMavenResourcesFiltering{
+public class ItFilter implements MavenResourcesFiltering {
 
+    @Override
+    public void filterResources(List<Resource> resources, File outputDirectory, MavenProject mavenProject, String encoding, List<String> fileFilters, List<String> nonFilteredFileExtensions, MavenSession mavenSession) throws MavenFilteringException {
+        throw new RuntimeException("not called");
+    }
+
+    @Override
+    public void filterResources(List<Resource> resources, File outputDirectory, String encoding, List<FileUtils.FilterWrapper> filterWrappers, File resourcesBaseDirectory, List<String> nonFilteredFileExtensions) throws MavenFilteringException {
+        throw new RuntimeException("not called");
+    }
+
+    @Override
+    public List getDefaultNonFilteredFileExtensions() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean filteredFileExtension(String fileName, List<String> userNonFilteredFileExtensions) {
+        return fileName.endsWith(".me");
+    }
 
     @Override
     public void filterResources(MavenResourcesExecution mavenResourcesExecution) throws MavenFilteringException {
+        List<Resource> resources = mavenResourcesExecution.getResources();
+        for (Resource resource : resources) {
+            System.out.println(resource);
+        }
+
         System.out.println();
+        System.out.println("maven Resource Execution"+mavenResourcesExecution);
         System.out.println("move along nothing to see");
         System.out.println();
-        super.filterResources(mavenResourcesExecution);
     }
 }
