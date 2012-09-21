cd21a2bd593cfaac97cea320828e09971a0becf4
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2012-09-21 08:53:24 +0200

    Added Sandbox for logging with sl4fj

---
+++ sandbox-slf4j/src/main/java/com/github/signed/ClassWithLogging.java
@@ -0,0 +1,12 @@
+package com.github.signed;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ClassWithLogging {
+
+    public static void main(String [] args) {
+        Logger logger = LoggerFactory.getLogger(ClassWithLogging.class);
+        logger.debug("hello you fool");
+    }
+}
