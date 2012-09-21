fef3b621f0e14852e0b9ddcc671465d9a6b445ef
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2012-09-21 10:08:55 +0200

    RollingFileAppender from log4j extras configured via log4j.properties

--- sandbox-slf4j/src/main/java/com/github/signed/ClassWithLogging.java
+++ sandbox-slf4j/src/main/java/com/github/signed/ClassWithLogging.java
@@ -1,12 +1,20 @@
 package com.github.signed;
 
+import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Random;
+
 public class ClassWithLogging {
 
-    public static void main(String [] args) {
+    public static void main(String[] args) throws InterruptedException {
         Logger logger = LoggerFactory.getLogger(ClassWithLogging.class);
-        logger.debug("hello you fool");
+        Random random = new Random();
+        while (true) {
+            Thread.sleep(random.nextInt(10000));
+            System.out.println(new DateTime());
+            logger.debug("hello you fool");
+        }
     }
 }
