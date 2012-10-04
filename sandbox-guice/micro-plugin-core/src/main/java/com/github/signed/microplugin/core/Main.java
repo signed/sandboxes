2d7bc5633d2ba11256fda9fb276df1c86e9e1c58
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2012-10-04 09:00:14 +0200

    deleted dead code

--- sandbox-guice/micro-plugin-core/src/main/java/com/github/signed/microplugin/core/Main.java
+++
@@ -1,4 +0,0 @@
-package com.github.signed.microplugin.core;
-
-public class Main {
-}
--- sandbox-guice/micro-plugin-core/src/main/java/com/github/signed/microplugin/core/ModuleWithContributionSupport.java
+++ sandbox-guice/micro-plugin-core/src/main/java/com/github/signed/microplugin/core/ModuleWithContributionSupport.java
@@ -1,13 +1,6 @@
 package com.github.signed.microplugin.core;
 
-import com.github.signed.microplugin.core.ApplicationContributionModule;
 import com.google.inject.AbstractModule;
-import com.google.inject.multibindings.Multibinder;
 
 public abstract  class ModuleWithContributionSupport extends AbstractModule implements ApplicationContributionModule {
-
-    protected <ExtensionPoint>void contributeTo(Class<ExtensionPoint> extensionPointClass, Class<? extends ExtensionPoint> implementer){
-        Multibinder<ExtensionPoint> toolBarContributionMultibinder = Multibinder.newSetBinder(binder(), extensionPointClass);
-        toolBarContributionMultibinder.addBinding().to(implementer);
-    }
 }
