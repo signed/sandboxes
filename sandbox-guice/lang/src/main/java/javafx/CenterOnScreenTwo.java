5e91a2f8d4e9f7183aae6ba0f01a1c216cacaef2
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2012-10-15 19:29:47 +0200

    Added some common javafx stuff...

---
+++ sandbox-guice/lang/src/main/java/javafx/CenterOnScreenTwo.java
@@ -0,0 +1,23 @@
+package javafx;
+
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+public class CenterOnScreenTwo implements EventHandler<WindowEvent> {
+    @Override
+    public void handle(WindowEvent windowEvent) {
+        Stage stage = (Stage)windowEvent.getSource();
+        ObservableList<Screen> screens = Screen.getScreens();
+        Screen screen = screens.get(1);
+        Rectangle2D bounds = screen.getVisualBounds();
+
+        double x = bounds.getMinX() + (bounds.getMaxX() - bounds.getMinX()) / 2;
+        double y = bounds.getMinY() + (bounds.getMaxY() - bounds.getMinY()) / 2;
+        stage.setX(x);
+        stage.setY(y);
+    }
+}
