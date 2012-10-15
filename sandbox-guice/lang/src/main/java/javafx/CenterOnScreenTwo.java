80263280461ce1405c6bcd421ebbfae1d207c349
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2012-10-15 19:36:43 +0200

    CenterOnScreen cops with single screen setups

--- sandbox-guice/lang/src/main/java/javafx/CenterOnScreenTwo.java
+++ sandbox-guice/lang/src/main/java/javafx/CenterOnScreenTwo.java
@@ -10,14 +10,20 @@ import javafx.stage.WindowEvent;
 public class CenterOnScreenTwo implements EventHandler<WindowEvent> {
     @Override
     public void handle(WindowEvent windowEvent) {
-        Stage stage = (Stage)windowEvent.getSource();
-        ObservableList<Screen> screens = Screen.getScreens();
-        Screen screen = screens.get(1);
-        Rectangle2D bounds = screen.getVisualBounds();
-
+        Rectangle2D bounds = screen().getVisualBounds();
         double x = bounds.getMinX() + (bounds.getMaxX() - bounds.getMinX()) / 2;
         double y = bounds.getMinY() + (bounds.getMaxY() - bounds.getMinY()) / 2;
+        Stage stage = (Stage)windowEvent.getSource();
         stage.setX(x);
         stage.setY(y);
+    }
+
+    private Screen screen() {
+        ObservableList<Screen> screens = Screen.getScreens();
+        Screen screen = screens.get(0);
+        if(screens.size()>1) {
+            screen = screens.get(1);
+        }
+        return screen;
     }
 }
