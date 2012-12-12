358926b2e5dfa1fda8101ad2b6d4592c75f30c73
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2012-12-12 23:53:43 +0100

    added a first submodule

---
+++ sandbox-gradle/multi-module/logic/src/test/java/hallo/World_Test.java
@@ -0,0 +1,14 @@
+package hallo;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class World_Test {
+
+    @Test
+    public void createTheWorld() throws Exception {
+        assertThat(new World(), CoreMatchers.notNullValue());
+    }
+}
