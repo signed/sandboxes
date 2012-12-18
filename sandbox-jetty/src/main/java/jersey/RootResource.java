ddde34ef476f604cb3c6c02543e3b0bf0b3ac1eb
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2012-12-18 21:52:32 +0100

    The web-app configuration is loaded from web.xml entirely

--- sandbox-jetty/src/main/java/jersey/RootResource.java
+++ sandbox-jetty/src/main/java/jersey/RootResource.java
@@ -4,6 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Date;
 
 @Path("/")
 public class RootResource {
@@ -11,7 +12,7 @@ public class RootResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String helloWorld() {
-        return "Hello World";
+        return "Hello World "+new Date().getTime();
     }
 
     @Path("/below")
--- sandbox-jetty/src/main/java/jetty/JettyServer.java
+++ sandbox-jetty/src/main/java/jetty/JettyServer.java
@@ -1,12 +1,8 @@
 package jetty;
 
-import com.google.inject.servlet.GuiceFilter;
-import jersey.guice.GuiceConfig;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
-import org.eclipse.jetty.servlet.DefaultServlet;
-import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 public class JettyServer {
@@ -20,14 +16,6 @@ public class JettyServer {
 
     public static void main(String[] args) throws Exception {
         Server server = new Server(8182);
-
-        ServletContextHandler jerseyServlet = new ServletContextHandler();
-        jerseyServlet.setResourceBase("");
-        jerseyServlet.addEventListener(new GuiceConfig());
-        jerseyServlet.addFilter(GuiceFilter.class, "/*", null);
-        jerseyServlet.addServlet(DefaultServlet.class, "/");
-        jerseyServlet.setContextPath("/buggy");
-
 
         String contentRoot = JettyServer.class.getProtectionDomain().getCodeSource().getLocation().toExternalForm();
         System.out.println(contentRoot);
