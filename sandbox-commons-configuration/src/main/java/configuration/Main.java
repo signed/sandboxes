da30466c65d99317c5b07cc09c0ce898053b9b48
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2012-12-20 20:55:34 +0100

    added scm version

--- sandbox-commons-configuration/src/main/java/configuration/Main.java
+++ sandbox-commons-configuration/src/main/java/configuration/Main.java
@@ -1,7 +1,10 @@
 package configuration;
 
+import org.apache.commons.configuration.PropertiesConfiguration;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("hello world");
+        PropertiesConfiguration propertiesConfiguration = new PropertiesConfiguration();
     }
 }
