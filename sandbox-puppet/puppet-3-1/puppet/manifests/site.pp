ef18a7dc18c87bf282f619ac70add3a4154d7bed
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2013-02-14 15:35:38 +0100

    Added jenkins, provision does not work right now, failures in the jenkins module

--- sandbox-puppet/puppet-3-1/puppet/manifests/site.pp
+++ sandbox-puppet/puppet-3-1/puppet/manifests/site.pp
@@ -1,5 +1,33 @@
 node default {
-  package{ "puppet":
-    ensure=>latest,
-  }
+    include apt
+    include stdlib
+    include jenkins
+
+    package{ "puppet":
+        ensure=>latest,
+    }
+
+    $plugins = [
+      'git',
+      'chucknorris',
+      'github',
+      'greenballs',
+      'ruby',
+      'rake',
+      'ssh',
+      'monitoring',
+      'favorite',
+      'gravatar',
+      'jabber',
+      'sounds',
+      'radiatorviewplugin',
+      'vsphere-cloud',
+      'warnings',
+      'analysis-core',
+      'rvm',
+      'htmlpublisher',
+      'xfpanel'
+    ]
+
+    jenkins::plugin::install { $plugins: }
 }
