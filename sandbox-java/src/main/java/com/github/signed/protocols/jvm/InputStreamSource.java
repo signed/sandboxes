9b12da575032d9e71f1b632910030e92c2a7676c
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2013-02-15 19:20:12 +0100

    Deleted the jvm protocol handler code

--- sandbox-java/src/main/java/com/github/signed/protocols/jvm/InputStreamSource.java
+++
@@ -1,8 +0,0 @@
-package com.github.signed.protocols.jvm;
-
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-
-public interface InputStreamSource {
-    InputStream inputStream() throws FileNotFoundException;
-}
