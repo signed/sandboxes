e47f8e827e4004e4796cca9a1c193b98f1bd810b
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2013-03-03 20:30:00 +0100

    Examples for the *. operator

---
+++ sandbox-groovy/src/test/groovy/lists/Operators.groovy
@@ -0,0 +1,29 @@
+package lists
+import org.hamcrest.CoreMatchers
+import org.junit.Test
+
+import static org.hamcrest.MatcherAssert.assertThat
+
+class Operators {
+
+    private static class AClass{
+        String attribute
+    }
+
+    private static class AnotherClass{
+    }
+
+
+    @Test
+    public void extractAPropertyFromAllElementsOfTheListAndReturnIt() throws Exception {
+        def trimmed = [new AClass(attribute: 'one'), new AClass(attribute: 'two')]*.attribute
+        assertThat(trimmed, CoreMatchers.is(['one', 'two']))
+    }
+
+    @Test(expected = MissingFieldException.class)
+    public void ifAnElementDoesNotHaveTheRequiredField() throws Exception {
+        def trimmed = [new AClass(attribute: 'one'), new AnotherClass()]*.attribute
+        assertThat(trimmed, CoreMatchers.is(['one']))
+    }
+
+}
