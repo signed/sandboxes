db42aaa7413fd3aa5c577ae9b3c2329c1e8ef821
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2013-03-19 13:39:09 +0100

    A test with an immutable object

--- sandbox-java/src/main/java/serialization/ImmutableObject.java
+++ sandbox-java/src/main/java/serialization/ImmutableObject.java
@@ -1,12 +1,13 @@
 package serialization;
 
-public class ImmutableObject {
+import java.io.Serializable;
+
+public class ImmutableObject implements Serializable {
     private final String stuff;
 
     public ImmutableObject(String stuff) {
         this.stuff = stuff;
     }
-
 
     public String getStuff() {
         return this.stuff;
