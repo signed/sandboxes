2829ae3377d1be0f3448697137db47b9183c585e
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2013-03-20 15:26:06 +0100

    Push service via IceFaces

--- sandbox-jee/src/main/java/beans/Chat.java
+++ sandbox-jee/src/main/java/beans/Chat.java
@@ -1,9 +1,38 @@
 package beans;
 
+import org.icefaces.application.PushRenderer;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
 
 @ApplicationScoped
 @Named
 public class Chat {
+    private String message;
+    private List<String> messages = new ArrayList<String>();
+
+    public void addMessage() {
+        messages.add(message);
+        message = "";
+        PushRenderer.render("messages");
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<String> getMessages() {
+        PushRenderer.addCurrentSession("messages");
+        return messages;
+    }
+
+    public void setMessages(List<String> messages) {
+        this.messages = messages;
+    }
 }
