54f929b5139137046b36d66864c919e170a1ba9d
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2013-04-27 18:04:38 +0200

    Extended the cdi example for http request parameter injection

--- sandbox-jee/plain-jsf/src/main/java/view/parameter/cdi/CdiParameter.java
+++ sandbox-jee/plain-jsf/src/main/java/view/parameter/cdi/CdiParameter.java
@@ -1,5 +1,6 @@
 package view.parameter.cdi;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.logging.Logger;
@@ -12,7 +13,19 @@ public class CdiParameter {
 
     @Inject
     @HttpParam("token")
-    public String token = "Initial Cdi token assigned at field declaration";
+    private String token = "Initial Cdi token assigned at field declaration";
+    private String tokenPostConstruct;
+
+    @Inject
+    @HttpParam("another")
+    private String bogus = "Initial value assigned to bogus at field declaration";
+    private String bogusPostConstruct;
+
+    @PostConstruct
+    private void postConstruct() {
+        tokenPostConstruct = token;
+        bogusPostConstruct = bogus;
+    }
 
     public void setToken(String newContent) {
         logger.severe("setToken( "+newContent+" )");
@@ -22,5 +35,29 @@ public class CdiParameter {
     public String getToken() {
         logger.severe("getToken()");
         return this.token;
+    }
+
+    public String getTokenPostConstruct() {
+        return tokenPostConstruct;
+    }
+
+    public void setTokenPostConstruct(String tokenPostConstruct) {
+        this.tokenPostConstruct = tokenPostConstruct;
+    }
+
+    public String getBogus() {
+        return bogus;
+    }
+
+    public void setBogus(String bogus) {
+        this.bogus = bogus;
+    }
+
+    public String getBogusPostConstruct() {
+        return bogusPostConstruct;
+    }
+
+    public void setBogusPostConstruct(String bogusPostConstruct) {
+        this.bogusPostConstruct = bogusPostConstruct;
     }
 }
