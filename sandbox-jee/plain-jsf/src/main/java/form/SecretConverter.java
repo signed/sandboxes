fa9692c424b9a297a449a8820f8c6ba0eab834ba
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2013-04-27 20:43:13 +0200

    Display a custom text if conversion failed

--- sandbox-jee/plain-jsf/src/main/java/form/SecretConverter.java
+++ sandbox-jee/plain-jsf/src/main/java/form/SecretConverter.java
@@ -1,14 +1,23 @@
 package form;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
 
 @FacesConverter("converter.secrets")
 public class SecretConverter implements Converter {
     @Override
     public Object getAsObject(FacesContext facesContext, UIComponent uiComponent, String s) {
+        if( null == s || s.isEmpty()) {
+            return s;
+        }
+        if("bunny".equals(s)){
+            FacesMessage message = new FacesMessage( FacesMessage.SEVERITY_ERROR, "boring", "Thats no secret");
+            throw new ConverterException(message);
+        }
         return "[Las Vegas] "+s;
     }
 
