2c03c604096d41a8c3ae52dd619e894005b0c3cf
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2013-04-27 16:59:22 +0200

    Injecting a logger with cdi

--- sandbox-jee/plain-jsf/src/main/java/view/parameter/cdi/CdiParameter.java
+++ sandbox-jee/plain-jsf/src/main/java/view/parameter/cdi/CdiParameter.java
@@ -1,16 +1,24 @@
 package view.parameter.cdi;
 
+import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.logging.Logger;
 
 @Named("cdi")
 public class CdiParameter {
+
+    @Inject
+    private Logger logger;
+
     public String content = "Initial Cdi content assigned at field declaration";
 
     public void setContent(String newContent) {
+        logger.severe("setContent( "+newContent+" )");
         this.content = newContent;
     }
 
     public String getContent() {
+        logger.severe("getContent()");
         return this.content;
     }
 }
