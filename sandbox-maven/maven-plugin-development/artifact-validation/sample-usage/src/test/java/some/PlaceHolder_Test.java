42c88ba8a916cbf7b8f1552f78168260068ee16b
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2013-05-02 10:46:51 +0200

    The surefire plugin is executed in the validate phase

--- sandbox-maven/maven-plugin-development/artifact-validation/sample-usage/src/test/java/some/PlaceHolder_Test.java
+++ sandbox-maven/maven-plugin-development/artifact-validation/sample-usage/src/test/java/some/PlaceHolder_Test.java
@@ -1,13 +1,21 @@
 package some;
 
-import org.hamcrest.CoreMatchers;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class PlaceHolder_Test {
     @Test
     public void aDummyTest() throws Exception {
-        assertThat(new PlaceHolder(), CoreMatchers.notNullValue());
+        assertThat(new PlaceHolder(), notNullValue());
+    }
+
+    @Test
+    @Ignore
+    public void fail() throws Exception {
+        assertThat(true, is(false));
     }
 }
