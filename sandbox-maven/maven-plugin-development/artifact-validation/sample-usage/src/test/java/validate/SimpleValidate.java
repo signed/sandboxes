ae8e84252b93bd04d7aa23ea2f3e5861b4ca112d
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2013-05-02 11:28:08 +0200

    implemented a license check

--- sandbox-maven/maven-plugin-development/artifact-validation/sample-usage/src/test/java/validate/SimpleValidate.java
+++ sandbox-maven/maven-plugin-development/artifact-validation/sample-usage/src/test/java/validate/SimpleValidate.java
@@ -1,18 +1,16 @@
 package validate;
 
 import org.junit.Test;
+import org.zeroturnaround.zip.ZipUtil;
+
+import java.io.File;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class SimpleValidate {
     @Test
     public void testName() throws Exception {
-        blankLine();
         String artifact = System.getProperties().getProperty("artifact", "not present");
-        System.out.println("validate up an running: "+artifact);
-        blankLine();
-
-    }
-
-    private void blankLine() {
-        System.out.println();
+        assertThat("Artifact contains a license file", ZipUtil.containsEntry(new File(artifact), "LICENSE"));
     }
 }
