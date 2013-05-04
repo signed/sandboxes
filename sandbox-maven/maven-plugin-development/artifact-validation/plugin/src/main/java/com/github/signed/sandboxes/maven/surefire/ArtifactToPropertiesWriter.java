22e0dde87cb83149de2fbdc1da26894ef67319f3
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2013-05-04 11:58:07 +0200

    The artifacts are written to a properties file

--- sandbox-maven/maven-plugin-development/artifact-validation/plugin/src/main/java/com/github/signed/sandboxes/maven/surefire/ArtifactToPropertiesWriter.java
+++ sandbox-maven/maven-plugin-development/artifact-validation/plugin/src/main/java/com/github/signed/sandboxes/maven/surefire/ArtifactToPropertiesWriter.java
@@ -1,7 +1,9 @@
 package com.github.signed.sandboxes.maven.surefire;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Properties;
 
 public class ArtifactToPropertiesWriter {
     private final File rootDirectory;
@@ -10,7 +12,32 @@ public class ArtifactToPropertiesWriter {
         this.rootDirectory = rootDirectory;
     }
 
-    public void write() throws IOException {
-        new File(this.rootDirectory, "artifacts.properties").createNewFile();
+    public void write(Iterable<Stuff> artifacts) throws IOException {
+        Properties properties = new Properties();
+        populateProperties(artifacts, properties);
+        writeProperties(properties);
+    }
+
+    private void populateProperties(Iterable<Stuff> artifacts, Properties properties) {
+        for (Stuff artifact : artifacts) {
+            String key = "maven.artifact" + (null == artifact.classifier()? "":"."+artifact.classifier());
+            properties.put(key, artifact.location().getAbsolutePath());
+        }
+    }
+
+    private void writeProperties(Properties properties) throws IOException {
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(outputFile());
+            properties.store(writer, "Create by the artifact-validation-maven-plugin");
+        } finally {
+            if (null != writer) {
+                writer.close();
+            }
+        }
+    }
+
+    private File outputFile() {
+        return new File(this.rootDirectory, "artifacts.properties");
     }
 }
--- sandbox-maven/maven-plugin-development/artifact-validation/plugin/src/test/java/com/github/signed/sandboxes/maven/surefire/ArtifactToPropertiesWriter_Test.java
+++ sandbox-maven/maven-plugin-development/artifact-validation/plugin/src/test/java/com/github/signed/sandboxes/maven/surefire/ArtifactToPropertiesWriter_Test.java
@@ -1,12 +1,19 @@
 package com.github.signed.sandboxes.maven.surefire;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
+import static org.hamcrest.CoreMatchers.endsWith;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -14,6 +21,7 @@ public class ArtifactToPropertiesWriter_Test {
 
     @Rule
     public final TemporaryFolder folder = new TemporaryFolder();
+    private final List<Stuff> artifacts = new ArrayList<Stuff>();
 
     private ArtifactToPropertiesWriter writer;
     private File rootDirectory;
@@ -26,11 +34,29 @@ public class ArtifactToPropertiesWriter_Test {
 
     @Test
     public void writePropertiesFile() throws Exception {
-        writer.write();
+        writer.write(artifacts);
         assertThat(new File(rootDirectory, "artifacts.properties").exists(), is(true));
     }
 
     @Test
-    public void writeTheArtifactsToTheFile() throws Exception {
+    public void writeTheArtifactToFile() throws Exception {
+        artifacts.add(DummyArtifact.defaultArtifactAt("secret-location"));
+        writer.write(artifacts);
+
+        assertThat(readProperty("maven.artifact"), CoreMatchers.endsWith("secret-location"));
+    }
+
+    @Test
+    public void appendTheClassifierToThePropertyKey() throws Exception {
+        artifacts.add(DummyArtifact.attachedArtifactWith("classic", "some-place"));
+        writer.write(artifacts);
+
+        assertThat(readProperty("maven.artifact.classic"), endsWith("some-place"));
+    }
+
+    private String readProperty(String key) throws IOException {
+        Properties properties = new Properties();
+        properties.load(new FileInputStream(new File(rootDirectory, "artifacts.properties")));
+        return properties.getProperty(key);
     }
 }
