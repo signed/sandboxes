90a88f254cb6aba459085d9f09ef5aaf373f3719
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2013-06-07 17:40:38 +0200

    Put the configuration file on the classpath

--- sandbox-maven/cargo-plugin/the-war/src/main/java/com/github/signed/maven/ReadProperties.java
+++ sandbox-maven/cargo-plugin/the-war/src/main/java/com/github/signed/maven/ReadProperties.java
@@ -1,6 +1,7 @@
 package com.github.signed.maven;
 
 import javax.inject.Named;
+import java.net.URL;
 
 @Named("integrationtest")
 public class ReadProperties {
@@ -8,7 +9,12 @@ public class ReadProperties {
     private String available = "catch me if you can";
 
     public String getAvailable(){
-        return available;
+        URL configuration = ReadProperties.class.getResource("/configuration.properties");
+        if(configuration == null){
+            return "not available";
+        }else {
+            return configuration.toExternalForm();
+        }
     }
 
     public void setAvailable(String available) {
