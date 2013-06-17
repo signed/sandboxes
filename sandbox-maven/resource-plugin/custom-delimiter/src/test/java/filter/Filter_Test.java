f31c1b4e2148961ff6ae2ba09e7119e6a9d1a5f7
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2013-06-17 10:33:28 +0200

    Restructure sanbox sample

---
+++ sandbox-maven/resource-plugin/custom-delimiter/src/test/java/filter/Filter_Test.java
@@ -0,0 +1,25 @@
+package filter;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class Filter_Test {
+    @Test
+    public void replaceThePattern() throws Exception {
+        assertThat(filteredTestFile(), containsString("Hallo @Thomas"));
+    }
+
+    private String filteredTestFile() throws IOException {
+        URL resource = Filter_Test.class.getResource("/filter.test");
+        byte[] raw = Files.readAllBytes(Paths.get(resource.getPath()));
+        return new String(raw, Charset.forName("UTF-8"));
+    }
+}
