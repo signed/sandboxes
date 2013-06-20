f89690ea0c3573aff74d51eaadfd57c29e2c0dc6
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2013-06-20 12:16:44 +0200

    Access a bean in the ActionListener implementation

--- sandbox-jee/plain-jsf/src/main/java/form/GossipSource.java
+++ sandbox-jee/plain-jsf/src/main/java/form/GossipSource.java
@@ -1,13 +1,17 @@
 package form;
 
+import utils.Utils;
+
 import javax.faces.event.AbortProcessingException;
 import javax.faces.event.ActionEvent;
 import java.util.logging.Logger;
 
 public class GossipSource implements javax.faces.event.ActionListener {
     private final Logger logger = Logger.getLogger(GossipSource.class.getName());
+
     @Override
     public void processAction(ActionEvent actionEvent) throws AbortProcessingException {
-        logger.info("some told us a secret!");
+        FormBean formBean = Utils.getManagedBean("formData");
+        logger.info(formBean.getName() + " told us a secret!");
     }
 }
---
+++ sandbox-jee/plain-jsf/src/main/java/utils/Utils.java
@@ -0,0 +1,27 @@
+package utils;
+
+import javax.el.ELContext;
+import javax.faces.FacesException;
+import javax.faces.context.FacesContext;
+
+public class Utils {
+    public static <T>  T getManagedBean(final String beanName) {
+        FacesContext fc = FacesContext.getCurrentInstance();
+        Object bean;
+
+        try {
+            ELContext elContext = fc.getELContext();
+            bean = elContext.getELResolver().getValue(elContext, null, beanName);
+        } catch (RuntimeException e) {
+            throw new FacesException(e.getMessage(), e);
+        }
+
+        if (bean == null) {
+            throw new FacesException("Managed bean with name '" + beanName
+                    + "' was not found. Check your faces-config.xml or @ManagedBean annotation.");
+        }
+
+        return (T)bean;
+    }
+
+}
