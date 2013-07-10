c6321ec9a57936757e41869c568b1c28c2917ff5
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2013-07-10 10:37:09 +0200

    Added new sample for a bug report

---
+++ sandbox-intellij/extract-parameter-dialog/src/main/java/menomic/AllOccurrences.java
@@ -0,0 +1,14 @@
+package menomic;
+
+public class AllOccurrences {
+
+    private final String field = "not important";
+
+    public void callerMethod(){
+        calledMethod();
+    }
+
+    private void calledMethod(){
+        System.out.println("first: "+ field + " second: "+field);
+    }
+}
