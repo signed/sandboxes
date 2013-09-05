58bb977b2336e8516d917c4872e939a387b4585b
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2013-09-05 16:58:11 +0200

    Parsing my own class

--- sandbox-jython/src/main/jython/AstFun.py
+++ sandbox-jython/src/main/jython/AstFun.py
@@ -1,13 +1,13 @@
 __author__ = 'signed'
 
+import tokenize
 
-import tokenize, cStringIO
-
-source = "{'test':'123','hehe':['hooray',0x10]}"
-src = cStringIO.StringIO(source).readline
 
 print 'running...'
-result = tokenize.generate_tokens(src)
+
+source = open("HelloWorld.py", 'r').read()
 
 for t in tokenize.generate_tokens(iter([source]).next):
     tokenize.printtoken(*t)
+
+
