8b4976c203b68ce3b8df0a7ee9108953eb93c1e0
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2013-10-08 22:45:26 +0200

    Reproduced, that mappings from other jars are not loaded

--- sandbox-jpa/hibernate/src/test/java/com/github/signed/sandbox/jpa/Embedded_Test.java
+++ sandbox-jpa/hibernate/src/test/java/com/github/signed/sandbox/jpa/Embedded_Test.java
@@ -1,6 +1,7 @@
 package com.github.signed.sandbox.jpa;
 
 import org.h2.tools.Server;
+import org.hibernate.hql.ast.QuerySyntaxException;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -32,10 +33,49 @@ public class Embedded_Test {
     public void basicSetupForInMemoryH2Database() throws Exception {
         Server server = Server.createTcpServer("-tcpPort", "9081", "-tcpAllowOthers");
         server.start();
-        writeWithJdbc();
+        writeIntoTable("DEMO");
         List<Demo> demos = readWithHibernate();
-        assertThat(demos.get(0).getComment(), is("hello"));
+        assertThat(demos.get(0).getComment(), is("hello DEMO"));
         server.shutdown();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void loadHibernateMappingsFromAnotherJar() throws Exception {
+        Server server = Server.createTcpServer("-tcpPort", "9082", "-tcpAllowOthers");
+        server.start();
+        writeIntoTable("WORLD");
+        List<World> worlds = readWithHibernateMappingsInAnotherArtifact();
+        assertThat(worlds.get(0).getComment(), is( "hello WORLD"));
+        server.shutdown();
+    }
+
+    private List<World> readWithHibernateMappingsInAnotherArtifact() {
+        Properties overridePropertiesFromPersistenceXml = new Properties();
+        overridePropertiesFromPersistenceXml.setProperty("javax.persistence.jdbc.driver", "org.h2.Driver");
+        overridePropertiesFromPersistenceXml.setProperty("javax.persistence.jdbc.url", jdbcUrlBuilder.buildUrl());
+        overridePropertiesFromPersistenceXml.setProperty("javax.persistence.jdbc.user", userName);
+        overridePropertiesFromPersistenceXml.setProperty("javax.persistence.jdbc.password", password);
+        overridePropertiesFromPersistenceXml.setProperty("hibernate.show_sql", "false");
+        overridePropertiesFromPersistenceXml.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("the-demo-unit", overridePropertiesFromPersistenceXml);
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = null;
+        try {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            TypedQuery<World> query = entityManager.createQuery("select w from World w", World.class);
+            List<World> worlds = query.getResultList();
+            transaction.commit();
+            return worlds;
+        } catch (RuntimeException ex) {
+            if (null != transaction) {
+                transaction.rollback();
+            }
+            StringWriter stringWriter = new StringWriter();
+            ex.printStackTrace(new PrintWriter(stringWriter));
+            throw ex;
+        }
     }
 
     private List<Demo> readWithHibernate() {
@@ -68,14 +108,14 @@ public class Embedded_Test {
         throw new UnreachableCodeException();
     }
 
-    private void writeWithJdbc() throws SQLException {
+    private void writeIntoTable(String tableName) throws SQLException {
         String jdbcUrl = jdbcUrlBuilder.buildUrl();
         System.out.println("jdbcUrl = " + jdbcUrl);
         Connection connection = DriverManager.getConnection(jdbcUrl, "sa", "sa");
         Statement statement = connection.createStatement();
-        statement.execute("CREATE TABLE DEMO(ID INT PRIMARY KEY, comment VARCHAR)");
-        statement.execute("INSERT INTO DEMO (id, comment) VALUES (1, 'hello') ");
-        ResultSet resultSet = statement.executeQuery("SELECT * from DEMO");
+        statement.execute("CREATE TABLE "+tableName+"(ID INT PRIMARY KEY, comment VARCHAR)");
+        statement.execute("INSERT INTO "+tableName+" (id, comment) VALUES (1, 'hello "+tableName+"') ");
+        ResultSet resultSet = statement.executeQuery("SELECT * from "+tableName);
         while (resultSet.next()) {
             int id = resultSet.getInt(1);
             String comment = resultSet.getString(2);
