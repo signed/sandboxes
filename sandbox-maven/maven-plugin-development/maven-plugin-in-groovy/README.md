6d0e02569e46ed6ca41b27f8ef05d17743e492fb
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2013-10-10 00:13:51 +0200

    Junit Tests are running in groovy

--- sandbox-maven/maven-plugin-development/maven-plugin-in-groovy/README.md
+++ sandbox-maven/maven-plugin-development/maven-plugin-in-groovy/README.md
@@ -3,3 +3,5 @@ http://blog.anorakgirl.co.uk/2013/04/writing-a-maven-3-plugin-in-groovy/
 https://maven.apache.org/plugin-tools/maven-plugin-plugin/examples/using-annotations.html
 
 http://stackoverflow.com/questions/11209382/build-maven-plugins-using-gradle
+
+https://github.com/spockframework/spock-example
--- sandbox-maven/maven-plugin-development/maven-plugin-in-groovy/logic-in-groovy/src/test/groovy/com/github/signed/sandbox/maven/plugin/LogicTest.groovy
+++ sandbox-maven/maven-plugin-development/maven-plugin-in-groovy/logic-in-groovy/src/test/groovy/com/github/signed/sandbox/maven/plugin/LogicTest.groovy
@@ -1,4 +1,19 @@
 package com.github.signed.sandbox.maven.plugin
 
+import org.junit.Test
+
+import static org.hamcrest.CoreMatchers.equalTo
+import static org.hamcrest.MatcherAssert.assertThat
+
 class LogicTest {
+
+    @Test
+    void 'do the funny stuff'(){
+        assertThat(new Logic().executeLogic(), equalTo('The logic tells you 42'))
+    }
+
+    @Test
+    void 'do the funny stuff again but call it different'(){
+        assertThat(new Logic().executeLogic(), equalTo('The logic tells you 42'))
+    }
 }
