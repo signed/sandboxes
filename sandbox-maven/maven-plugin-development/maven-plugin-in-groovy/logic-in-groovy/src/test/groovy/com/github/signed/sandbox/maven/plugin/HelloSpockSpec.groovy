f5ef2366231e84428dcfe816ff652e187f720b0b
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2013-10-10 00:44:09 +0200

    Added missing specification

---
+++ sandbox-maven/maven-plugin-development/maven-plugin-in-groovy/logic-in-groovy/src/test/groovy/com/github/signed/sandbox/maven/plugin/HelloSpockSpec.groovy
@@ -0,0 +1,16 @@
+package com.github.signed.sandbox.maven.plugin
+
+import spock.lang.Specification;
+
+class HelloSpockSpec extends Specification {
+    def "length of Spock's and his friends' names"() {
+        expect:
+        name.size() == length
+
+        where:
+        name     | length
+        "Spock"  | 5
+        "Kirk"   | 4
+        "Scotty" | 6
+    }
+}
