d37948a918af36a4e3598d0def5b34662c5f29a7
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2013-11-21 21:32:24 +0100

    First test

--- sandbox-pmd/pmd-contributions/src/main/java/com/github/signed/pmd/abstractions/MethodCallExtractor.java
+++ sandbox-pmd/pmd-contributions/src/main/java/com/github/signed/pmd/abstractions/MethodCallExtractor.java
@@ -1,8 +1,15 @@
 package com.github.signed.pmd.abstractions;
 
+import com.beust.jcommander.internal.Lists;
 import net.sourceforge.pmd.lang.java.ast.JavaParserVisitorAdapter;
 
+import java.util.List;
+
 public class MethodCallExtractor extends JavaParserVisitorAdapter {
+    private final List<MethodCall> methodCalls = Lists.newArrayList();
 
-
+    public List<MethodCall> methods() {
+        methodCalls.add(new MethodCall());
+        return methodCalls;
+    }
 }
--- sandbox-pmd/pmd-contributions/src/test/java/com/github/signed/pmd/abstractions/MethodCallExtractor_Test.java
+++ sandbox-pmd/pmd-contributions/src/test/java/com/github/signed/pmd/abstractions/MethodCallExtractor_Test.java
@@ -1,13 +1,18 @@
 package com.github.signed.pmd.abstractions;
 
 import com.beust.jcommander.internal.Maps;
+import com.github.signed.pmd.AstParser;
 import com.sun.codemodel.CodeWriter;
 import com.sun.codemodel.JCodeModel;
 import com.sun.codemodel.JDefinedClass;
 import com.sun.codemodel.JExpr;
 import com.sun.codemodel.JMethod;
 import com.sun.codemodel.JPackage;
+import net.sourceforge.pmd.PMDException;
+import net.sourceforge.pmd.lang.java.ast.JavaNode;
+import org.hamcrest.Matchers;
 import org.javacc.parser.JavaCCParser;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -15,24 +20,38 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Map;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class MethodCallExtractor_Test {
-    @Test
-    public void testName() throws Exception {
-        JCodeModel model = new JCodeModel();
+
+    private final JCodeModel model = new JCodeModel();
+    private final MethodCallExtractor extractor = new MethodCallExtractor();
+
+    @Before
+    public void setUp() throws Exception {
         JDefinedClass singleton = model._class("singletons.Singleton");
         JMethod instanceMethod = singleton.method(JavaCCParser.ModifierSet.PUBLIC | JavaCCParser.ModifierSet.STATIC, singleton, "instance");
         instanceMethod.body()._return(JExpr._new(singleton));
 
-        JDefinedClass singletonAccessor = model._class("apackage.DoNotCare");
+        JDefinedClass singletonAccessor = model._class("apackage.SingletonAccess");
         JMethod methodWithSingletonAccess = singletonAccessor.method(JavaCCParser.ModifierSet.PUBLIC, model.VOID, "doStuff");
         methodWithSingletonAccess.body().add(singleton.staticInvoke(instanceMethod));
 
+    }
+
+    @Test
+    public void testName() throws Exception {
+        extractMethodCallsInClass("SingletonAccess");
+
+        assertThat(extractor.methods(), Matchers.hasSize(1));
+    }
+
+    private void extractMethodCallsInClass(String className) throws IOException, PMDException {
         InMemoryCodeWriter writer = new InMemoryCodeWriter();
-
         model.build(writer);
+        JavaNode classWithSingletonAccess = new AstParser().parse(writer.getSourceForClass(className));
 
-        System.out.println(writer.getSourceForClass("DoNotCare"));
-        System.out.println(writer.getSourceForClass("Singleton"));
+        classWithSingletonAccess.jjtAccept(extractor, null);
     }
 
     public static class InMemoryCodeWriter extends CodeWriter {
