8caabba4a51a96bf7583d53d7266dde1909b19b9
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2013-11-27 22:11:05 +0100

    Using the new Singleton rule to find errors

--- sandbox-pmd/pmd-contributions/src/main/java/com/github/signed/pmd/rules/SingletonAccess.java
+++ sandbox-pmd/pmd-contributions/src/main/java/com/github/signed/pmd/rules/SingletonAccess.java
@@ -1,4 +1,20 @@
 package com.github.signed.pmd.rules;
 
-public class SingletonAccess {
+import com.github.signed.pmd.abstractions.MethodCallExtractor;
+import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceDeclaration;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
+
+public class SingletonAccess extends AbstractJavaRule{
+
+    @Override
+    public Object visit(ASTClassOrInterfaceDeclaration node, Object data) {
+        try{
+        MethodCallExtractor methodCallExtractor = new MethodCallExtractor();
+        node.jjtAccept(methodCallExtractor, data);
+        }catch(Exception any){
+            addViolationWithMessage(data, node, "exception");
+        }
+        return data;
+    }
+
 }
