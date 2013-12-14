0594ac17b92cb2f506c0ed49538409fdb0dd23f8
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2013-12-14 21:00:34 +0100

    Setup a simple root with ftp an jms

--- sandbox-enterprise-integration/camel/src/main/java/com/github/signed/integration/camel/Camel.java
+++ sandbox-enterprise-integration/camel/src/main/java/com/github/signed/integration/camel/Camel.java
@@ -3,6 +3,7 @@ package com.github.signed.integration.camel;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.file.remote.FtpComponent;
 import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 
@@ -13,11 +14,12 @@ public class Camel {
         ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
 
         CamelContext context = new DefaultCamelContext();
+        context.addComponent("ftp", new FtpComponent(context));
         context.addComponent("jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
         context.addRoutes(new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                from("ftp://").to("jms:queuq:out");
+                from("ftp://localhost:10021/boon?username=sally&password=secret").to("jms:out");
             }
         });
 
