2e3bcf4ed60db86f95520804bbee5fbf4108f29f
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2013-12-14 16:51:29 +0100

    Setup a JMS-Server with active mq

---
+++ sandbox-enterprise-integration/infrastructure/jms/src/main/java/infrastructure/jms/Start.java
@@ -0,0 +1,21 @@
+package infrastructure.jms;
+
+import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.broker.TransportConnector;
+
+import java.net.URI;
+
+public class Start {
+
+    public static void main(String[] args) throws Exception {
+        BrokerService brokerService = new BrokerService();
+        brokerService.setBrokerName("in-memory");
+        brokerService.setPersistent(false);
+
+        TransportConnector connector = new TransportConnector();
+        connector.setUri(new URI("tcp://localhost:61616"));
+        brokerService.addConnector(connector);
+
+        brokerService.start();
+    }
+}
