6b3e3cdc1e9d8fa2a003e5a4e538d80b241af089
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2014-01-15 21:17:20 +0100

    added a sample how to deploy war to jboss

---
+++ sandbox-gradle/jboss-deploy/README.md
@@ -0,0 +1,6 @@
+https://github.com/michel-kraemer/gradle-download-task
+
+
+
+http://www.gradle.org/docs/current/userguide/working_with_files.html
+http://www.gradle.org/docs/current/userguide/tutorial_this_and_that.html
---
+++ sandbox-gradle/jboss-deploy/gradle-lib-directory/src/main/java/apackage/AClass.java
@@ -0,0 +1,12 @@
+package apackage;
+
+import org.joda.time.DateTime;
+
+public class AClass{
+
+    public static void main(String [] args)throws Exception {
+        DateTime dateTime = new DateTime();
+        System.out.println("dateTime = " + dateTime);
+    }
+
+}
