fc3ece75dd7a40c1463932ac2cf1d03a49d0e846
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2014-03-23 10:16:07 +0100

    First dozer example up and running
    hat to exclude log4j

---
+++ sandbox-dozer/src/main/java/beans/PrimitiveTypes.java
@@ -0,0 +1,40 @@
+package beans;
+
+public class PrimitiveTypes {
+    private String string;
+    private int _int;
+    private long _long;
+    private double _double;
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public int get_int() {
+        return _int;
+    }
+
+    public void set_int(int _int) {
+        this._int = _int;
+    }
+
+    public long get_long() {
+        return _long;
+    }
+
+    public void set_long(long _long) {
+        this._long = _long;
+    }
+
+    public double get_double() {
+        return _double;
+    }
+
+    public void set_double(double _double) {
+        this._double = _double;
+    }
+}
---
+++ sandbox-dozer/src/test/java/beans/PrimitiveTypes_Test.java
@@ -0,0 +1,39 @@
+package beans;
+
+import org.dozer.DozerBeanMapper;
+import org.dozer.loader.api.BeanMappingBuilder;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class PrimitiveTypes_Test {
+
+    @Test
+    public void testName() throws Exception {
+        PrimitiveTypes primitiveTypes = new PrimitiveTypes();
+        primitiveTypes.set_double(3.3);
+        primitiveTypes.set_int(42);
+        primitiveTypes.set_long(23);
+        primitiveTypes.setString("Hello Dozer");
+
+        BeanMappingBuilder builder = new BeanMappingBuilder() {
+            @Override
+            protected void configure() {
+                mapping(PrimitiveTypes.class, PrimitiveTypes.class);
+            }
+        };
+
+        DozerBeanMapper mapper = new DozerBeanMapper();
+        mapper.addMapping(builder);
+
+
+
+        PrimitiveTypes mapped = new PrimitiveTypes();
+        mapped = mapper.map(primitiveTypes, PrimitiveTypes.class);
+        assertThat(mapped.getString(), is("Hello Dozer"));
+        assertThat(mapped.get_double(), is(3.3));
+        assertThat(mapped.get_int(), is(42));
+        assertThat(mapped.get_long(), is(23l));
+    }
+}
