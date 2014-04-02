0f5ba610ebce2ab906e09a06f1764e5156ba8655
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2014-04-02 23:14:34 +0200

    test cleanup

--- sandbox-ruby/wischan/spec/wischan_spec.rb
+++ sandbox-ruby/wischan/spec/wischan_spec.rb
@@ -1,9 +1,8 @@
 require 'spec_helper'
 
 describe Wischan do
-  it "should return data for a named element" do
+  it "should greet the person confronted with" do
     greeting = Wischan.greet('developer')
-    greeting.should_not be_nil
     greeting.should == 'Hello developer'
   end
 end
