6fcc24301159563c9b251b440a1c8782c37cb431
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2014-04-04 13:34:45 +0200

    Added a sample root

--- sandbox-enterprise-integration/camel/src/main/java/com/github/signed/integration/camel/CamelEvaluationCenter.java
+++ sandbox-enterprise-integration/camel/src/main/java/com/github/signed/integration/camel/CamelEvaluationCenter.java
@@ -1,6 +1,7 @@
 package com.github.signed.integration.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 import com.github.signed.integration.camel.gui.CamelContextIgnition;
 import com.github.signed.integration.camel.gui.swing.CamelEvaluationCenterSwing;
@@ -16,8 +17,28 @@ public class CamelEvaluationCenter {
     private void run() {
         CamelEvaluationCenterSwing gui = new CamelEvaluationCenterSwing();
         gui.constructApplicationFrame();
-        CamelContext context = new DefaultCamelContext();
+        CamelContext context = createCamelContext();
+
+        addRoutesTo(context);
         new CamelContextIgnition(context, gui.startStop());
         gui.start();
+    }
+
+    private void addRoutesTo(CamelContext context) {
+        try {
+            context.addRoutes(new RouteBuilder() {
+                @Override
+                public void configure() throws Exception {
+                    from("file:camel/src/main/resources/?noop=true&fileName=sample.txt").routeId("welcome wagon").to("stream:out");
+                }
+            });
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private CamelContext createCamelContext() {
+        DefaultCamelContext context = new DefaultCamelContext();
+        return context;
     }
 }
--- sandbox-enterprise-integration/camel/src/main/java/com/github/signed/integration/camel/FtpUpload.java
+++ sandbox-enterprise-integration/camel/src/main/java/com/github/signed/integration/camel/FtpUpload.java
@@ -1,9 +1,9 @@
 package com.github.signed.integration.camel;
 
 import org.apache.camel.CamelContext;
-import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.file.remote.FtpComponent;
+import org.apache.camel.component.stream.StreamComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class FtpUpload {
@@ -11,6 +11,7 @@ public class FtpUpload {
     public static void main(String[] args) throws Exception {
         CamelContext context = new DefaultCamelContext();
         context.addComponent("ftp", new FtpComponent(context));
+        context.addComponent("stream", new StreamComponent());
         context.addRoutes(new RouteBuilder() {
             @Override
             public void configure() throws Exception {
@@ -18,10 +19,10 @@ public class FtpUpload {
                         .log("Uploading file ${file:name}")
                         .multicast().parallelProcessing().to("ftp://localhost:10021?username=sally&password=secret&delay=2500", "ftp://localhost:10021?username=harry&password=secret&delay=2500")
                         .log("Uploaded file ${file:name} complete.");
+
+                from("file:camel/src/main/resources/?noop=true&fileName=sample.txt").log("GOTCHA").routeId("print text file").to("stream:out");
             }
         });
-
-        ProducerTemplate producerTemplate = context.createProducerTemplate();
 
         context.start();
         Thread.sleep(100000);
