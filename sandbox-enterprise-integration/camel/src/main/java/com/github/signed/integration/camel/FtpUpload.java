7ef5bee1f67288cdd6e5a16df47480c685a0f9c4
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2014-04-04 15:52:39 +0200

    Sample for downloading a file from http

--- sandbox-enterprise-integration/camel/src/main/java/com/github/signed/integration/camel/FtpUpload.java
+++ sandbox-enterprise-integration/camel/src/main/java/com/github/signed/integration/camel/FtpUpload.java
@@ -1,6 +1,7 @@
 package com.github.signed.integration.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.file.remote.FtpComponent;
 import org.apache.camel.component.stream.StreamComponent;
@@ -12,7 +13,35 @@ public class FtpUpload {
         CamelContext context = new DefaultCamelContext();
         context.addComponent("ftp", new FtpComponent(context));
         context.addComponent("stream", new StreamComponent());
+        //context.addRoutes(ftpMulticast());
         context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:trigger-download").to("http://scm:8081/nexus/content/repositories/releases/de/idos/operatingreserve/moccamote-application/1.3.0/moccamote-application-1.3.0.pom.sha1").to("stream:out");
+            }
+        });
+        context.addRoutes(directSystemOut());
+
+
+        context.start();
+        ProducerTemplate template = context.createProducerTemplate();
+        template.sendBody("direct:trigger-download", null);
+        //template.sendBody("direct:system-out", new URI("http://scm:8081/nexus/content/repositories/releases/de/idos/operatingreserve/moccamote-application/1.3.0/moccamote-application-1.3.0.pom"));
+        Thread.sleep(100000);
+        context.stop();
+    }
+
+    private static RouteBuilder directSystemOut() {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:system-out").to("stream:out");
+            }
+        };
+    }
+
+    private static RouteBuilder ftpMulticast() {
+        return new RouteBuilder() {
             @Override
             public void configure() throws Exception {
                 from("file:/home/signed/dev/arbeitskopie/customers/build/files/?noop=true")
@@ -21,11 +50,8 @@ public class FtpUpload {
                         .log("Uploaded file ${file:name} complete.");
 
                 from("file:camel/src/main/resources/?noop=true&fileName=sample.txt").log("GOTCHA").routeId("print text file").to("stream:out");
-            }
-        });
 
-        context.start();
-        Thread.sleep(100000);
-        context.stop();
+            }
+        };
     }
 }
