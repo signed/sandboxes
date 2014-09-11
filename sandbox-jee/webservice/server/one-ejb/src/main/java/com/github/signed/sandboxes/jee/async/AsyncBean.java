4c14061d9859664d10f8349dff09108e5e4987c4
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2014-09-12 00:37:03 +0200

    Async with future seems to work, if jboss feels like injecting is ok

--- sandbox-jee/webservice/server/one-ejb/src/main/java/com/github/signed/sandboxes/jee/async/AsyncBean.java
+++ sandbox-jee/webservice/server/one-ejb/src/main/java/com/github/signed/sandboxes/jee/async/AsyncBean.java
@@ -1,17 +1,23 @@
 package com.github.signed.sandboxes.jee.async;
 
+import java.util.concurrent.Future;
+
+import javax.ejb.AsyncResult;
 import javax.ejb.Asynchronous;
 import javax.ejb.Stateless;
 
 @Stateless
 public class AsyncBean {
- 
+
     @Asynchronous
-    public void asyncMethod() {
+    public Future<String> asyncMethod() {
         System.out.println("start running asyncMethod in thread " + Thread.currentThread().getName());
         try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {  e.printStackTrace();  }
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println("finished running asyncMethod in thread " + Thread.currentThread().getName());
+        return new AsyncResult<>("the result");
     }
 }
--- sandbox-jee/webservice/server/one-war/src/main/java/com/github/signed/sandboxes/jee/webservlet/AsyncTestServlet.java
+++ sandbox-jee/webservice/server/one-war/src/main/java/com/github/signed/sandboxes/jee/webservlet/AsyncTestServlet.java
@@ -1,6 +1,8 @@
 package com.github.signed.sandboxes.jee.webservlet;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -17,12 +19,14 @@ public class AsyncTestServlet extends HttpServlet {
     @Inject
     AsyncBean asyncBean;
 
-
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("caller method running in thread " + Thread.currentThread().getName());
-        asyncBean.asyncMethod();
-        response.getWriter().append("Hello you");
+        Future<String> future = asyncBean.asyncMethod();
+        try {
+            response.getWriter().append(future.get());
+        } catch (InterruptedException | ExecutionException e) {
+            response.getWriter().append("fail");
+        }
         System.out.println("caller method returned in thread " + Thread.currentThread().getName());
     }
 }
