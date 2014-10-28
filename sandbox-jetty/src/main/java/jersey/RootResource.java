35db95e5229e50464e593e2c22f19d366ae0de8f
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2014-10-28 20:56:13 +0100

    formating

--- sandbox-jetty/src/main/java/jersey/RootResource.java
+++ sandbox-jetty/src/main/java/jersey/RootResource.java
@@ -1,10 +1,11 @@
 package jersey;
 
+import java.util.Date;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.Date;
 
 @Path("/")
 public class RootResource {
@@ -12,7 +13,7 @@ public class RootResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String helloWorld() {
-        return "Hello World "+new Date().getTime();
+        return "Hello World " + new Date().getTime();
     }
 
     @Path("/below")
