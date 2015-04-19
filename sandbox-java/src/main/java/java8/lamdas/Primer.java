01dda4e322a38c49d2c0f0e295e9df9d6210ace9
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-04-19 17:29:41 +0200

    basic lambda samples

---
+++ sandbox-java/src/main/java/java8/lamdas/Primer.java
@@ -0,0 +1,38 @@
+package java8.lamdas;
+
+public class Primer {
+
+    public static int compareStrings(String first, String second) {
+        return first.compareTo(second);
+    }
+
+    public static Primer CreateWith(String content){
+        return new Primer(content);
+    }
+
+    private final String content;
+
+    public Primer(String content) {
+        this.content = content;
+    }
+
+    public int someCompareMethod(Primer primer) {
+        return this.content.compareTo(primer.content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Primer primer = (Primer) o;
+
+        return !(content != null ? !content.equals(primer.content) : primer.content != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return content != null ? content.hashCode() : 0;
+    }
+}
---
+++ sandbox-java/src/test/java/java8/lamdas/Primer_Test.java
@@ -0,0 +1,39 @@
+package java8.lamdas;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+public class Primer_Test {
+
+    @Test
+    public void useStaticMethodAsComparator() throws Exception {
+        List<String> someStrings = new ArrayList<>();
+        someStrings.add("b");
+        someStrings.add("a");
+        someStrings.add("c");
+
+        Collections.sort(someStrings, Primer::compareStrings);
+
+        assertThat(someStrings, is(Arrays.asList("a", "b", "c")));
+    }
+
+    @Test
+    public void useAMethodOfOneOfTheInstances() throws Exception {
+        List<Primer> somePrimers = new ArrayList<>();
+        somePrimers.add(Primer.CreateWith("a"));
+        somePrimers.add(Primer.CreateWith("d"));
+        somePrimers.add(Primer.CreateWith("c"));
+
+        Collections.sort(somePrimers, Primer::someCompareMethod);
+
+        assertThat(somePrimers, is(Arrays.asList(Primer.CreateWith("a"), Primer.CreateWith("c"), Primer.CreateWith("d"))));
+
+    }
+}
