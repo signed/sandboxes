31408e4e13249641e2f8ce94fce8b5e6cb1ef6d4
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-05-12 23:17:09 +0200

    Added another example

--- sandbox-spring/resttemplate/src/test/java/com/github/signed/sandbox/spring/resttemplate/DoNotTryToParseResponseEntityOnErrorTest.java
+++ sandbox-spring/resttemplate/src/test/java/com/github/signed/sandbox/spring/resttemplate/DoNotTryToParseResponseEntityOnErrorTest.java
@@ -6,6 +6,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,7 +16,7 @@ import spark.Spark;
 
 public class DoNotTryToParseResponseEntityOnErrorTest {
 
-    public static class TransferObject{
+    public static class TransferObject {
         public String value;
     }
 
@@ -26,14 +27,17 @@ public class DoNotTryToParseResponseEntityOnErrorTest {
     @Before
     public void setUp() throws Exception {
         Spark.port(8085);
-        Spark.get("/transferobject", "application/json",  (request, response) -> {
+        Spark.get("/transferobject", (request, response) -> {
             TransferObject result = new TransferObject();
             result.value = "Hello Again";
             response.type("application/json");
             return result;
         }, model -> {
+            //return model.toString();
             return new ObjectMapper().writeValueAsString(model);
         });
+
+        Thread.sleep(500);
     }
 
     @After
@@ -41,11 +45,22 @@ public class DoNotTryToParseResponseEntityOnErrorTest {
         Spark.stop();
     }
 
+    final String url = "http://localhost:8085/transferobject";
+
     @Test
     public void getWithSpringRestTemplateEntity() throws Exception {
-        String url = "http://localhost:8085/transferobject";
-        RestTemplate spring = new RestTemplate();
+        final RestTemplate spring = new RestTemplate();
+
         ResponseEntity<TransferObject> responseEntity = spring.getForEntity(url, TransferObject.class);
+
+        assertThat(responseEntity.getBody().value, is("Hello Again"));
+    }
+
+    @Test
+    public void testName() throws Exception {
+        final RestTemplate spring = new RestTemplate();
+
+        ResponseEntity<TransferObject> responseEntity = spring.exchange(url, HttpMethod.GET, null, TransferObject.class);
 
         assertThat(responseEntity.getBody().value, is("Hello Again"));
     }
