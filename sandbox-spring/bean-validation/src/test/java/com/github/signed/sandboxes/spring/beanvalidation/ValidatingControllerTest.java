d73d66ec811ef80dcbe2ffe8ded75056bba23244
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-07-27 22:50:29 +0200

    incorporated new findings

--- sandbox-spring/bean-validation/src/test/java/com/github/signed/sandboxes/spring/beanvalidation/ValidatingControllerTest.java
+++ sandbox-spring/bean-validation/src/test/java/com/github/signed/sandboxes/spring/beanvalidation/ValidatingControllerTest.java
@@ -1,15 +1,16 @@
 package com.github.signed.sandboxes.spring.beanvalidation;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import org.junit.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
 import retrofit.RestAdapter;
 import retrofit.client.Response;
 import retrofit.http.Body;
 import retrofit.http.PUT;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ValidatingControllerTest {
     private static class TransferObject {
@@ -23,7 +24,8 @@ public class ValidatingControllerTest {
 
     @Test(expected = RuntimeException.class)
     public void testName() throws Exception {
-        ConfigurableApplicationContext context = SpringApplication.run(BootWithBeanValidationApplication.class);
+        SpringApplication springApplication = new SpringApplication(BootWithBeanValidationApplication.class);
+        ConfigurableApplicationContext context = springApplication.run();
 
         RestAdapter restAdapter = new RestAdapter.Builder()
                 .setEndpoint("http://localhost:8080")
@@ -33,6 +35,7 @@ public class ValidatingControllerTest {
         Response putResponse = client.put(new TransferObject());
 
         assertThat(putResponse.getStatus(), is(400));
+
         SpringApplication.exit(context);
     }
 }
