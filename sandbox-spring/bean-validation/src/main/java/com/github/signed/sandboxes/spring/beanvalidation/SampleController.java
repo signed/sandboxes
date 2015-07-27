b3e9e525e9c6adc51757209d665196b995c89f62
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-07-27 15:49:33 +0200

    started a bean validation sample

---
+++ sandbox-spring/bean-validation/src/main/java/com/github/signed/sandboxes/spring/beanvalidation/SampleController.java
@@ -0,0 +1,17 @@
+package com.github.signed.sandboxes.spring.boot;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@EnableAutoConfiguration
+public class SampleController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String home(BeanToValidate bean) {
+        return "Hello World!";
+    }
+}
