92606e0b4aee01514c3728a6dbc239546d50eab5
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-08-09 20:15:22 +0200

    Expose the validator. This way it can be @Autowired into other beans

--- sandbox-spring/bean-validation/src/main/java/com/github/signed/sandboxes/spring/beanvalidation/BootWithBeanValidationApplication.java
+++ sandbox-spring/bean-validation/src/main/java/com/github/signed/sandboxes/spring/beanvalidation/BootWithBeanValidationApplication.java
@@ -3,6 +3,8 @@ package com.github.signed.sandboxes.spring.beanvalidation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -10,5 +12,12 @@ public class BootWithBeanValidationApplication {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(BootWithBeanValidationApplication.class, args);
+    }
+
+    @Bean(name = "validator")
+    public LocalValidatorFactoryBean validator() {
+        LocalValidatorFactoryBean factory = new LocalValidatorFactoryBean();
+        //factory.setValidationMessageSource(messageSource());
+        return factory;
     }
 }
