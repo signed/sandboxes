fa18f5098384c37e06d834c99dc8d681eed49474
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-08-13 23:48:38 +0200

    Annotation is inherited from SpringBootApplication

--- sandbox-spring/boot-reactive/src/main/java/com/github/signed/sandboxes/spring/boot/caller/server/CallerBootApplication.java
+++ sandbox-spring/boot-reactive/src/main/java/com/github/signed/sandboxes/spring/boot/caller/server/CallerBootApplication.java
@@ -1,11 +1,9 @@
 package com.github.signed.sandboxes.spring.boot.caller.server;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class CallerBootApplication {
 
     public static void main(String[] args) throws Exception {
--- sandbox-spring/boot-reactive/src/main/java/com/github/signed/sandboxes/spring/boot/echo/server/DelayedEchoApplication.java
+++ sandbox-spring/boot-reactive/src/main/java/com/github/signed/sandboxes/spring/boot/echo/server/DelayedEchoApplication.java
@@ -1,11 +1,9 @@
 package com.github.signed.sandboxes.spring.boot.echo.server;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class DelayedEchoApplication {
     public static void main(String[] args) throws Exception {
         new SpringApplicationBuilder()
