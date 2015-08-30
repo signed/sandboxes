a6eb5a6cbdc85d45bb7ed8c5b51dfd97b06b1fcb
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-08-30 21:41:09 +0200

    ensure, that overridden bean is changed if production configuration changes

--- sandbox-spring/boot/src/main/java/com/github/signed/sandboxes/spring/boot/ControllerConfiguration.java
+++ sandbox-spring/boot/src/main/java/com/github/signed/sandboxes/spring/boot/ControllerConfiguration.java
@@ -1,13 +1,16 @@
 package com.github.signed.sandboxes.spring.boot;
 
+import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ControllerConfiguration {
 
-    @Bean
-    public Collaborator politeCollaborator() {
+    public static final String CollaboratorBeanName = "collaborator";
+
+    @Bean(name = CollaboratorBeanName, autowire = Autowire.BY_TYPE)
+    public Collaborator collaboratorFoo() {
         return new PoliteCollaborator();
     }
 
--- sandbox-spring/boot/src/test/java/com/github/signed/sandboxes/spring/boot/SpringStyleIntegrationTest.java
+++ sandbox-spring/boot/src/test/java/com/github/signed/sandboxes/spring/boot/SpringStyleIntegrationTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import retrofit.RestAdapter;
 
@@ -12,21 +13,29 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = BootApplication.class)
+@SpringApplicationConfiguration(classes = { BootApplication.class, SpringStyleIntegrationTest.Blub.class})
 @WebIntegrationTest({"server.port=0", "management.port=0"})
 public class SpringStyleIntegrationTest {
+
+
+    public static class Blub {
+        @Bean(name = ControllerConfiguration.CollaboratorBeanName)
+        public Collaborator wadHadeDudeDa() {
+            return () -> "replaced in the test";
+        }
+    }
 
     @Value("${local.server.port}")
     private int port;
 
     @Test
     public void returnProductionValue() throws Exception {
-        assertThat(Responses.readBodyAsUtf8String(client(Client.class).getInjected()), is("Hello kind person"));
+        assertThat(Responses.readBodyAsUtf8String(client(Client.class).getInjected()), is("replaced in the test"));
     }
 
     public <T> T client(Class<T> type) {
         RestAdapter restAdapter = new RestAdapter.Builder()
-                .setEndpoint("http://localhost:"+port)
+                .setEndpoint(String.format("http://localhost:%d", port))
                 .build();
         return restAdapter.create(type);
     }
