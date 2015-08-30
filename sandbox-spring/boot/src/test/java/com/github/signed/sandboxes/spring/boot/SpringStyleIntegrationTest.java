39ce8910a6719d8910e8e173fb08d99bd57d9eb4
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-08-30 17:58:19 +0200

    removed unneeded rest template

--- sandbox-spring/boot/src/test/java/com/github/signed/sandboxes/spring/boot/SpringStyleIntegrationTest.java
+++ sandbox-spring/boot/src/test/java/com/github/signed/sandboxes/spring/boot/SpringStyleIntegrationTest.java
@@ -1,18 +1,12 @@
 package com.github.signed.sandboxes.spring.boot;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.web.client.RestTemplate;
 import retrofit.RestAdapter;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -22,16 +16,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 @WebIntegrationTest({"server.port=0", "management.port=0"})
 public class SpringStyleIntegrationTest {
 
-    private final RestTemplate restTemplate = new TestRestTemplate();
     @Value("${local.server.port}")
     private int port;
-
-    private final Map<String, Object> parameters = new HashMap<>();
-
-    @Before
-    public void setUp() throws Exception {
-        parameters.put("port", port);
-    }
 
     @Test
     public void returnProductionValue() throws Exception {
