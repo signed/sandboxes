6dacb796f3ab850f0d673fdab2486f6dc9a55141
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-08-31 22:25:58 +0200

    merge commit

--- sandbox-spring/boot/src/test/java/com/github/signed/sandboxes/spring/boot/SampleControllerTest.java
+++ sandbox-spring/boot/src/test/java/com/github/signed/sandboxes/spring/boot/SampleControllerTest.java
@@ -1,36 +1,9 @@
 package com.github.signed.sandboxes.spring.boot;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Optional;
-
 import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExternalResource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.EmbeddedWebApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-
-import retrofit.RestAdapter;
 import retrofit.client.Response;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Optional;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -56,9 +29,4 @@ public class SampleControllerTest {
 
         assertThat(messageFromCollaborator, is("Hello kind person"));
     }
-
-    private static String readBodyAsUtf8String(Response response) throws IOException {
-        return new String(readBytesFrom(response.getBody().in()), "UTF-8");
-    }
-
 }
