7ce7c7f544256b58482273030b61964260296dd0
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-09-07 12:14:58 +0200

    Ignore by filter vs by annotation

--- sandbox-findbugs/maven-integration/src/main/java/com.github.signed.sanboxes.findbugs.violation/Violation.java
+++ sandbox-findbugs/maven-integration/src/main/java/com.github.signed.sanboxes.findbugs.violation/Violation.java
@@ -3,7 +3,7 @@ package com.github.signed.sanboxes.findbugs.violation;
 public class Violation {
     private final String[] arguments;
 
-    @edu.umd.cs.findbugs.annotations.SuppressFBWarnings("EI_EXPOSE_REP2")
+    //@edu.umd.cs.findbugs.annotations.SuppressFBWarnings("EI_EXPOSE_REP2")
     public Violation(String[] arguments) {
         this.arguments = arguments;
     }
