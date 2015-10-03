4f17bc2dc0854ef73dfbff86c98a083265a0ad09
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-10-03 13:09:48 +0200

    gray background if bamboo is not reachable

--- sandbox-dashing/local/bamboo/jobs/bamboopoll.rb
+++ sandbox-dashing/local/bamboo/jobs/bamboopoll.rb
@@ -1,4 +1,12 @@
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
+
+def plan_status
+  {
+      :bambooNotReachable => [true, false].sample,
+      :random_number => rand(1000)
+  }
+end
+
 SCHEDULER.every '1s', allow_overlapping: false do |job|
-  send_event('bamboo', { random_number: rand(1000) })
+  send_event('bamboo', plan_status())
 end
