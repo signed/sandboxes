4aef80badd882c3b22c86799d470e55dc98d91c5
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-10-04 22:32:40 +0200

    integrate with the code from the job

--- sandbox-dashing/local/scripts/script.rb
+++ sandbox-dashing/local/scripts/script.rb
@@ -14,6 +14,14 @@ class BuildOutcome
     @state = state
   end
 
+  def plan_name
+    @plan_name
+  end
+
+  def failed?
+    'Failed'.eql?(@state)
+  end
+
   def to_s
     "#{@plan_name} #{@key} -> #{@state}"
   end
@@ -64,23 +72,51 @@ class BambooRestClient
 end
 
 class JsonBuilder
+
+  def initialize
+    @json_dictionary = {
+        :bamboo_not_reachable => false,
+        :plan_does_not_exist => false,
+        :failed_plans => []
+    }
+  end
+
   def plan_does_not_exist
-    print 'plan not found'
+    # print 'plan not found'
+    @json_dictionary[:plan_dos_not_exist] = true
   end
 
   def could_not_connect_to_bamboo(exception)
-    print 'could not connect to bamboo'
-    print exception
+    # print 'could not connect to bamboo'
+    # print exception
+    @json_dictionary[:plan_does_not_exist] = true
   end
 
   def branch_build_outcomes(build_outcomes)
-    build_outcomes.each { |result| print result.to_s + "\n" }
+    # build_outcomes.each { |result| print result.to_s + "\n" }
+    banana = build_outcomes.select { |build_outcome| build_outcome.failed? }.map { |failed| failed_branch failed }
+    @json_dictionary[:failed_plans] = banana
   end
+
+  def json()
+    @json_dictionary
+  end
+
+  private
+  def failed_branch(build_outcome)
+    {
+        :name => build_outcome.plan_name,
+        :failed_because => 'just failed',
+        :link => 'http://localhost:3030/sample'
+    }
+  end
+
 end
 
 json_builder = JsonBuilder.new
 
 bamboo_rest_client = BambooRestClient.new('localhost', 6990)
 bamboo_rest_client.latest_build_outcome_for_all_branches('DAS-SAM', json_builder)
-bamboo_rest_client.latest_build_outcome_for_all_branches('DAS-SAD', json_builder)
+# bamboo_rest_client.latest_build_outcome_for_all_branches('DAS-SAD', json_builder)
+print json_builder.json
 
