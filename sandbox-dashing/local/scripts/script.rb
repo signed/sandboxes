de20fe0c570d324016a2658463af0f6e52603a63
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-10-04 21:35:44 +0200

    some refactoring

--- sandbox-dashing/local/scripts/script.rb
+++ sandbox-dashing/local/scripts/script.rb
@@ -3,10 +3,6 @@ require 'rest-client'
 require 'net/https'
 require 'json'
 
-response = RestClient.get 'http://localhost:6990/bamboo/rest/api/latest/result/DAS-SAM.json?expand=results%5B0%5D.result.plan.branches.branch.latestResult'
-
-json = JSON.parse(response)
-
 def pretty_print_json(json)
   print JSON.pretty_generate(json)
 end
@@ -23,18 +19,56 @@ class BuildOutcome
   end
 end
 
-def extract_build_outcome_from_json(json)
-  BuildOutcome.new json['planName'], json['key'], json['buildState']
+class BambooRestClient
+  def initialize(host, port)
+    @host = host
+    @port = port
+  end
+
+  def latest_build_outcome_for_all_branches(plan_key, callback)
+    build_status_url = "http://#{@host}:#{@port}/bamboo/rest/api/latest/result/#{plan_key}.json?expand=results%5B0%5D.result.plan.branches.branch.latestResult"
+    response = RestClient.get(build_status_url) { |response, _, _| response }
+    if response.code == 404
+      callback.plan_does_not_exist
+      return
+    end
+    json = JSON.parse(response)
+    all_branch_results(json, callback)
+    return
+  end
+
+  private
+  def all_branch_results(json, callback)
+    json_with_build_result_information = []
+    master_branch_json = json['results']['result'][0]
+    master_branch_json['planName'] = 'master'
+    json_with_build_result_information << master_branch_json
+    master_branch_json['plan']['branches']['branch'].each { |branch| json_with_build_result_information << branch['latestResult'] }
+    callback.branch_build_outcomes(json_with_build_result_information.map { |json| extract_build_outcome_from_json json })
+  end
+
+  def extract_build_outcome_from_json(json)
+    BuildOutcome.new json['planName'], json['key'], json['buildState']
+  end
 end
 
+class JsonBuilder
+  def plan_does_not_exist
+    print 'plan not found'
+  end
 
-def all_branch_results(json)
-  json_with_build_result_information = []
-  master_branch_json = json['results']['result'][0]
-  master_branch_json['planName'] = 'master'
-  json_with_build_result_information << master_branch_json
-  master_branch_json['plan']['branches']['branch'].each { |branch| json_with_build_result_information << branch['latestResult'] }
-  json_with_build_result_information.map { |json| extract_build_outcome_from_json json }
+  def could_not_connect_to_bamboo
+    print 'could not connect to bamboo'
+  end
+
+  def branch_build_outcomes(build_outcomes)
+    build_outcomes.each { |result| print result.to_s + "\n" }
+  end
 end
 
-all_branch_results(json).each { |result| print result.to_s + "\n" }
+json_builder = JsonBuilder.new
+
+bamboo_rest_client = BambooRestClient.new('localhost', 6990)
+bamboo_rest_client.latest_build_outcome_for_all_branches('DAS-SAM', json_builder)
+bamboo_rest_client.latest_build_outcome_for_all_branches('DAS-SAD', json_builder)
+
