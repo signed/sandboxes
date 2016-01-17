c5ab1d08cf37c3a48289c0faa11c8af1eead012c
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2016-01-17 15:18:01 +0100

    Use a dictionary instead of it then else

--- sandbox-openapiiniciative/swagger-parser/src/main/java/com/github/signed/swagger/Properties.java
+++ sandbox-openapiiniciative/swagger-parser/src/main/java/com/github/signed/swagger/Properties.java
@@ -2,6 +2,10 @@ package com.github.signed.swagger;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+import com.google.common.collect.Maps;
 
 import io.swagger.models.properties.IntegerProperty;
 import io.swagger.models.properties.Property;
@@ -10,15 +14,15 @@ import io.swagger.models.properties.StringProperty;
 
 public class Properties {
 
+    private final Map<Class<?>, Function<Property, List<DefinitionReference>>> definitionReference = Maps.newHashMap();
+
+    {
+        definitionReference.put(StringProperty.class, property -> Collections.emptyList());
+        definitionReference.put(IntegerProperty.class, property -> Collections.emptyList());
+        definitionReference.put(RefProperty.class, property -> Collections.singletonList((DefinitionReference) ((RefProperty) property)::getSimpleRef));
+    }
+
     public List<DefinitionReference> definitionReferencesIn(Property property) {
-        if (property instanceof RefProperty) {
-            return Collections.singletonList((DefinitionReference) ((RefProperty) property)::getSimpleRef);
-        }
-
-        if(property instanceof StringProperty || property instanceof IntegerProperty){
-            return Collections.emptyList();
-        }
-
-        throw new RuntimeException("Definition extraction from properties still needs work");
+        return definitionReference.get(property.getClass()).apply(property);
     }
 }
