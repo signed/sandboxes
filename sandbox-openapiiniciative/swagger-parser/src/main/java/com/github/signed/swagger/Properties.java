26ff557251f8c37a441785094b5a72ed471d65f7
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2016-01-17 15:19:29 +0100

    reduce duplication

--- sandbox-openapiiniciative/swagger-parser/src/main/java/com/github/signed/swagger/Properties.java
+++ sandbox-openapiiniciative/swagger-parser/src/main/java/com/github/signed/swagger/Properties.java
@@ -14,11 +14,15 @@ import io.swagger.models.properties.StringProperty;
 
 public class Properties {
 
+    private static Function<Property, List<DefinitionReference>> containsNoDefinitionReferences() {
+        return property -> Collections.emptyList();
+    }
+
     private final Map<Class<?>, Function<Property, List<DefinitionReference>>> definitionReference = Maps.newHashMap();
 
     {
-        definitionReference.put(StringProperty.class, property -> Collections.emptyList());
-        definitionReference.put(IntegerProperty.class, property -> Collections.emptyList());
+        definitionReference.put(StringProperty.class, containsNoDefinitionReferences());
+        definitionReference.put(IntegerProperty.class, containsNoDefinitionReferences());
         definitionReference.put(RefProperty.class, property -> Collections.singletonList((DefinitionReference) ((RefProperty) property)::getSimpleRef));
     }
 
