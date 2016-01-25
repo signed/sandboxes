1e44d2bf2eae1eaa96773ca5daeb542af6b22496
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2016-01-25 21:26:39 +0100

    Sort model definti

--- sandbox-openapiiniciative/swagger-parser/src/main/java/com/github/signed/swagger/sort/SwaggerSort.java
+++ sandbox-openapiiniciative/swagger-parser/src/main/java/com/github/signed/swagger/sort/SwaggerSort.java
@@ -1,19 +1,40 @@
 package com.github.signed.swagger.sort;
 
+import static com.github.signed.swagger.essentials.Collectors2.toLinkedMap;
+import static java.util.Optional.ofNullable;
+
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import io.swagger.models.Model;
 import io.swagger.models.Swagger;
 import io.swagger.models.Tag;
 
 public class SwaggerSort {
 
+    private final Comparator<String> comparator = (o1, o2) -> o1.toLowerCase().compareTo(o2.toLowerCase());
+
     public Swagger sort(Swagger swagger){
         swagger.setTags(sortedTags(swagger));
+        swagger.setDefinitions(sortDefinitions(swagger));
         return swagger;
     }
 
+    private Map<String, Model> sortDefinitions(Swagger swagger) {
+        if( null == swagger.getDefinitions()){
+            return null;
+        }
+        return swagger.getDefinitions().entrySet().stream().sorted((o1, o2) -> comparator.compare(o1.getKey(), o2.getKey())).collect(toLinkedMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
     private List<Tag> sortedTags(Swagger swagger) {
-        return swagger.getTags().stream().sorted((o1, o2) -> o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase())).collect(Collectors.toList());
+        Optional<List<Tag>> maybeTags = ofNullable(swagger.getTags());
+        if(!maybeTags.isPresent()){
+            return null;
+        }
+        return maybeTags.get().stream().sorted((o1, o2) -> comparator.compare(o1.getName(), o2.getName())).collect(Collectors.toList());
     }
 }
--- sandbox-openapiiniciative/swagger-parser/src/test/java/com/github/signed/swagger/sort/SwaggerSort_Test.java
+++ sandbox-openapiiniciative/swagger-parser/src/test/java/com/github/signed/swagger/sort/SwaggerSort_Test.java
@@ -2,6 +2,7 @@ package com.github.signed.swagger.sort;
 
 import static com.github.signed.swagger.essentials.TagMatcher.tagNamed;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 import java.util.Iterator;
 
@@ -23,13 +24,26 @@ public class SwaggerSort_Test {
         builder.defineTag("Ant");
         builder.defineTag("aaa");
 
-        Swagger sorted = sort();
-
-        Iterator<Tag> tags = sorted.getTags().iterator();
+        Iterator<Tag> tags = sort().getTags().iterator();
         assertThat(tags.next(), tagNamed("aaa"));
         assertThat(tags.next(), tagNamed("Ant"));
         assertThat(tags.next(), tagNamed("ape"));
         assertThat(tags.next(), tagNamed("zebra"));
+    }
+
+    @Test
+    public void sort_model_definitions_by_case_insensitive_identifier() throws Exception {
+        builder.withModelDefinition("zebra");
+        builder.withModelDefinition("ape");
+        builder.withModelDefinition("Ant");
+        builder.withModelDefinition("aaa");
+
+        Iterator<String> definitions = sort().getDefinitions().keySet().iterator();
+
+        assertThat(definitions.next(), is("aaa"));
+        assertThat(definitions.next(), is("Ant"));
+        assertThat(definitions.next(), is("ape"));
+        assertThat(definitions.next(), is("zebra"));
     }
 
     private Swagger sort() {
