08dd90e0b4fa5dbb7cb614ad0bfeb057666245de
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2016-01-25 21:02:00 +0100

    refactor test

--- sandbox-openapiiniciative/swagger-parser/src/test/java/com/github/signed/swagger/sort/SwaggerSort_Test.java
+++ sandbox-openapiiniciative/swagger-parser/src/test/java/com/github/signed/swagger/sort/SwaggerSort_Test.java
@@ -14,23 +14,25 @@ import io.swagger.models.Swagger;
 import io.swagger.models.Tag;
 
 public class SwaggerSort_Test {
+    private final SwaggerBuilder builder = SwaggerMother.emptyApiDefinition();
 
     @Test
     public void sort_tags_by_case_insensitive_name() throws Exception {
-        SwaggerBuilder builder = SwaggerMother.emptyApiDefinition();
-
         builder.defineTag("zebra");
         builder.defineTag("ape");
         builder.defineTag("Ant");
         builder.defineTag("aaa");
 
-
-        Swagger sorted = new SwaggerSort().sort(builder.build());
+        Swagger sorted = sort();
 
         Iterator<Tag> tags = sorted.getTags().iterator();
         assertThat(tags.next(), tagNamed("aaa"));
         assertThat(tags.next(), tagNamed("Ant"));
         assertThat(tags.next(), tagNamed("ape"));
         assertThat(tags.next(), tagNamed("zebra"));
+    }
+
+    private Swagger sort() {
+        return new SwaggerSort().sort(builder.build());
     }
 }
