f5e811f132cc6d7142aef7d043c5fc68f7f5f684
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2016-01-26 22:29:29 +0100

    remove duplication in production code

--- sandbox-openapiiniciative/swagger-parser/src/main/java/com/github/signed/swagger/sort/SwaggerSort.java
+++ sandbox-openapiiniciative/swagger-parser/src/main/java/com/github/signed/swagger/sort/SwaggerSort.java
@@ -1,12 +1,10 @@
 package com.github.signed.swagger.sort;
 
 import static com.github.signed.swagger.essentials.Collectors2.toLinkedMap;
-import static java.util.Optional.ofNullable;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import io.swagger.models.Model;
@@ -18,7 +16,7 @@ public class SwaggerSort {
 
     private final Comparator<String> comparator = (o1, o2) -> o1.toLowerCase().compareTo(o2.toLowerCase());
 
-    public Swagger sort(Swagger swagger){
+    public Swagger sort(Swagger swagger) {
         swagger.setTags(sortedTags(swagger));
         swagger.setParameters(sortedParameters(swagger));
         swagger.setDefinitions(sortDefinitions(swagger));
@@ -26,24 +24,24 @@ public class SwaggerSort {
     }
 
     private Map<String, Parameter> sortedParameters(Swagger swagger) {
-        if( null == swagger.getParameters()){
-            return null;
-        }
-        return swagger.getParameters().entrySet().stream().sorted((o1, o2) -> comparator.compare(o1.getKey(), o2.getKey())).collect(toLinkedMap(Map.Entry::getKey, Map.Entry::getValue));
+        return sortByKey(swagger.getParameters());
     }
 
     private Map<String, Model> sortDefinitions(Swagger swagger) {
-        if( null == swagger.getDefinitions()){
+        return sortByKey(swagger.getDefinitions());
+    }
+
+    private <T> Map<String, T> sortByKey(Map<String, T> map) {
+        if (null == map) {
             return null;
         }
-        return swagger.getDefinitions().entrySet().stream().sorted((o1, o2) -> comparator.compare(o1.getKey(), o2.getKey())).collect(toLinkedMap(Map.Entry::getKey, Map.Entry::getValue));
+        return map.entrySet().stream().sorted((o1, o2) -> comparator.compare(o1.getKey(), o2.getKey())).collect(toLinkedMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
     private List<Tag> sortedTags(Swagger swagger) {
-        Optional<List<Tag>> maybeTags = ofNullable(swagger.getTags());
-        if(!maybeTags.isPresent()){
+        if (null == swagger.getTags()) {
             return null;
         }
-        return maybeTags.get().stream().sorted((o1, o2) -> comparator.compare(o1.getName(), o2.getName())).collect(Collectors.toList());
+        return swagger.getTags().stream().sorted((o1, o2) -> comparator.compare(o1.getName(), o2.getName())).collect(Collectors.toList());
     }
 }
