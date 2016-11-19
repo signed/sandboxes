c0df2cd4e99580aea221ccea50a89820257c0dac
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2016-11-19 15:12:54 +0100

    fix spelling error

--- sandbox-monitoring/data-generator/src/test/java/clients/DockerTest.java
+++ sandbox-monitoring/data-generator/src/test/java/clients/DockerTest.java
@@ -1,17 +1,17 @@
 package clients;
 
-import org.junit.Assume;
-import org.junit.Test;
-
 import static java.lang.System.getProperty;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Assume;
+import org.junit.Test;
 
 public class DockerTest {
 
     @Test
     public void name() throws Exception {
-        Assume.assumeThat(getProperty("os.name"), equalTo("linux"));
+        Assume.assumeThat(getProperty("os.name"), equalTo("Linux"));
         assertThat(new Docker().dockerIp(), equalTo("localhost"));
     }
 }
