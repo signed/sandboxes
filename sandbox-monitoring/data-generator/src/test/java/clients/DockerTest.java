81af14ecf74d717d747e3f2f0ee66fb96a006e69
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2016-11-19 14:50:05 +0100

    fix gradle build

--- sandbox-monitoring/data-generator/src/test/java/clients/DockerTest.java
+++ sandbox-monitoring/data-generator/src/test/java/clients/DockerTest.java
@@ -1,14 +1,15 @@
 package clients;
 
+import org.junit.Test;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-
-import org.junit.Test;
 
 public class DockerTest {
 
     @Test
     public void name() throws Exception {
+
         assertThat(new Docker().dockerIp(), equalTo("localhost"));
     }
 }
