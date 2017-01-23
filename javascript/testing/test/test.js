74181553d6b54e4535640b8c727921e384826317
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2017-01-23 23:41:02 +0100

    use before and after hooks

--- javascript/testing/test/test.js
+++ javascript/testing/test/test.js
@@ -2,8 +2,28 @@ import {assert} from "chai";
 
 describe('Array', () => {
   describe('#indexOf()', () => {
+    before(function() {
+      console.log('before')
+    });
+
+    after(function() {
+      console.log('after')
+    });
+
+    beforeEach(function() {
+      console.log('before each')
+    });
+
+    afterEach(function() {
+      console.log('after each')
+    });
+
     it('should return -1 when the value is not present', () => {
       assert.equal(-1, [1, 2, 3].indexOf(4));
+    });
+
+    it('should return the index when the value is not present', () => {
+      assert.equal(1, [1, 2, 3].indexOf(2));
     });
   });
 });
