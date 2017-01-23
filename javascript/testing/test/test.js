4fa1611c8b7916a5b165443eb1d98483454edb8f
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2017-01-23 22:53:12 +0100

    use es6 imports for chai

--- javascript/testing/test/test.js
+++ javascript/testing/test/test.js
@@ -1,9 +1,9 @@
-var assert = require('assert');
+import {assert} from "chai";
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1, 2, 3].indexOf(4));
     });
   });
 });
