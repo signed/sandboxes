cdae2d828006ea820e1a31202538a5ebac028b15
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2017-02-07 23:53:15 +0100

    only log if key is available in mdc

---
+++ java/spring/spring-boot-extension/extension/src/main/java/com/github/signed/boot/logfilter/MDCStructuralConverter.java
@@ -0,0 +1,47 @@
+package com.github.signed.boot.logfilter;
+
+import ch.qos.logback.classic.pattern.ClassicConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+import java.util.Map;
+
+import static ch.qos.logback.core.util.OptionHelper.extractDefaultReplacement;
+
+public class MDCStructuralConverter extends ClassicConverter {
+    private String key;
+    private String name;
+
+    @Override
+    public void start() {
+        String[] keyInfo = extractDefaultReplacement(getFirstOption());
+        key = keyInfo[0];
+        if (keyInfo[1] == null) {
+            name = key;
+        } else {
+            name = keyInfo[1];
+        }
+        super.start();
+    }
+
+    @Override
+    public void stop() {
+        key = null;
+        name = null;
+        super.stop();
+    }
+
+    @Override
+    public String convert(ILoggingEvent event) {
+        Map<String, String> mdcPropertyMap = event.getMDCPropertyMap();
+
+        if (key == null || mdcPropertyMap == null || !mdcPropertyMap.containsKey(key)) {
+            return "";
+        }
+
+        String value = mdcPropertyMap.get(key);
+        if (value == null) {
+            return "";
+        }
+        return name + "=" + value;
+    }
+}
