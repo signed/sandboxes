125f8bd5ecb8ed4353120cb25cd36d4e5485ff72
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2017-02-08 23:23:34 +0100

    remove dead code

--- java/spring/spring-boot-extension/extension/src/main/java/com/github/signed/boot/environment/AdjustLogPatternPostProcessor.java
+++ java/spring/spring-boot-extension/extension/src/main/java/com/github/signed/boot/environment/AdjustLogPatternPostProcessor.java
@@ -23,35 +23,4 @@ public class AdjustLogPatternPostProcessor implements EnvironmentPostProcessor {
     }
 
     private static final String PROPERTY_SOURCE_NAME = "defaultProperties";
-
-    public void postPdrocessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-        Map<String, Object> map = new HashMap<String, Object>();
-        // This doesn't work with all logging systems but it's a useful default so you see
-        // traces in logs without having to configure it.
-        map.put("logging.pattern.level",
-                "%clr(%5p) %clr([${spring.application.name:},%X{X-B3-TraceId:-},%X{X-B3-SpanId:-},%X{X-Span-Export:-}]){yellow}");
-        map.put("spring.aop.proxyTargetClass", "true");
-        addOrReplace(environment.getPropertySources(), map);
-    }
-
-    private void addOrReplace(MutablePropertySources propertySources, Map<String, Object> map) {
-        MapPropertySource target = null;
-        if (propertySources.contains(PROPERTY_SOURCE_NAME)) {
-            PropertySource<?> source = propertySources.get(PROPERTY_SOURCE_NAME);
-            if (source instanceof MapPropertySource) {
-                target = (MapPropertySource) source;
-                for (String key : map.keySet()) {
-                    if (!target.containsProperty(key)) {
-                        target.getSource().put(key, map.get(key));
-                    }
-                }
-            }
-        }
-        if (target == null) {
-            target = new MapPropertySource(PROPERTY_SOURCE_NAME, map);
-        }
-        if (!propertySources.contains(PROPERTY_SOURCE_NAME)) {
-            propertySources.addLast(target);
-        }
-    }
 }
