d49ab3f86462bccee2ff080e1cce3b301d06af43
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2017-02-25 23:22:58 +0100

    use html webpack plugin to generate a html file

--- sandbox-java/src/main/resources/webpack/webpack.config.js
+++ sandbox-java/src/main/resources/webpack/webpack.config.js
@@ -1,11 +1,14 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
     entry: './app/index.js',
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [new HtmlWebpackPlugin()],
+    devtool: 'source-map'
 };
 
 module.exports = config;
