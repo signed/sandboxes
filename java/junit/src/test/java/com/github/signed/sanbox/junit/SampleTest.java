5aacd30fb840b31c06505e75ad466948a900bcc1
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2017-07-01 13:52:29 +0200

    working on getting the log information I want

--- java/junit/src/test/java/com/github/signed/sanbox/junit/SampleTest.java
+++ java/junit/src/test/java/com/github/signed/sanbox/junit/SampleTest.java
@@ -1,12 +1,31 @@
 package com.github.signed.sanbox.junit;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.log4j.Logger;
 import org.junit.Rule;
 import org.junit.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.InterceptingClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RequestCallback;
+import org.springframework.web.client.ResponseExtractor;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 
 public class SampleTest {
 
-    static final String DEMO_OUTPUT = "demo-output";
+    static final java.lang.String DEMO_OUTPUT = "demo-output";
 
     @Rule
     public PrintTestMethodNameRule printTestMethodNameRule = new PrintTestMethodNameRule();
@@ -18,14 +37,37 @@ public class SampleTest {
         String message = CaseFormat.SENTENCE.to(CaseFormat.LOWER_CAMEL, "here we go into the other direction");
         logger.warn(message);
 
-        String json = "{\n" +
-                "  \"one\": 34,\n" +
-                "  \"two\": \"banana\",\n" +
-                "  \"deep\": {\n" +
-                "    \"three\": \"mountains\",\n" +
-                "    \"four\": null\n" +
-                "  }\n" +
-                "}";
-        logger.warn(json);
+        new TinyHttpServer().startServer();
+
+        TransferObject transferObject = new TransferObject();
+        transferObject.one = "one";
+        transferObject.two = new Deep();
+        transferObject.two.acme = "RoadRunner";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(transferObject);
+        logger.info(json);
+
+        String url = "http://localhost:8000/test";
+
+
+        RestTemplate restTemplate = new RestTemplate(new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()));
+        ResponseEntity<TransferObject> response = restTemplate.exchange(url, POST, new HttpEntity<>(transferObject), TransferObject.class);
+        //ResponseEntity<TransferObject> response = restTemplate.exchange("http://localhost:8000/test", POST, new HttpEntity<>(json), TransferObject.class);
+
+
+        assertThat(response.getBody().three, equalTo("three"));
     }
+
+    public static class TransferObject {
+        public String one;
+        public Deep two;
+        public String three;
+    }
+
+    public static class Deep {
+        public String acme;
+        public String coyote;
+    }
+
 }
---
+++ java/junit/src/test/java/com/github/signed/sanbox/junit/TinyHttpServer.java
@@ -0,0 +1,57 @@
+package com.github.signed.sanbox.junit;
+
+import com.google.common.io.ByteStreams;
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.Collections;
+
+import static java.util.stream.Collectors.joining;
+
+public class TinyHttpServer {
+
+    public static void main(String[] args) throws IOException {
+        new TinyHttpServer().startServer();
+    }
+
+    public void startServer() throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/test", new MyHandler());
+        server.start();
+    }
+
+    private static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            soutRequest(exchange);
+            sendResponse(exchange);
+        }
+
+        private void soutRequest(HttpExchange exchange) throws IOException {
+            Headers requestHeaders = exchange.getRequestHeaders();
+            requestHeaders.forEach((key, value) -> System.out.println(key + ": " + value.stream().collect(joining(","))));
+
+            byte[] bytes = ByteStreams.toByteArray(exchange.getRequestBody());
+            String jsonBody = new String(bytes, "UTF-8");
+
+            System.out.println("request body \n" + jsonBody);
+        }
+
+        private void sendResponse(HttpExchange exchange) throws IOException {
+            String response = "{\n" +
+                    "  \"three\": \"three\"\n" +
+                    "}";
+            exchange.getResponseHeaders().put("Content-Type", Collections.singletonList("application/json"));
+            exchange.sendResponseHeaders(200, response.length());
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
