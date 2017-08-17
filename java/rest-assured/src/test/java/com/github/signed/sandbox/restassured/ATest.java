678444cee9aca8748577913e6e7f752f25865ac0
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2017-08-17 21:57:01 +0200

    some samples asserting json bodies

--- java/rest-assured/src/test/java/com/github/signed/sandbox/restassured/ATest.java
+++ java/rest-assured/src/test/java/com/github/signed/sandbox/restassured/ATest.java
@@ -1,8 +1,10 @@
 package com.github.signed.sandbox.restassured;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.jayway.restassured.path.json.JsonPath;
 import com.jayway.restassured.response.Response;
 import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -10,7 +12,11 @@ import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.jayway.restassured.RestAssured.expect;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.hasItem;
 
 public class ATest {
 
@@ -19,16 +25,48 @@ public class ATest {
 
     @Test
     public void basicExample() throws Exception {
-        server.stubFor(get(urlEqualTo("/health"))
-                .willReturn(aResponse().withHeader("content-type", "application/json")
-                        .withStatus(200)));
+        stubHealthJsonToReturn("{}");
 
         Response response = expect()
                 .statusCode(200)
                 .when()
                 .get("health");
 
+        assertThat(response.getStatusCode(), equalTo(200));
+    }
 
-        assertThat(response.getStatusCode(), CoreMatchers.equalTo(200));
+    @Test
+    public void assertJsonBody() throws Exception {
+        stubHealthJsonToReturn("{\n" +
+                "  \"one\": 1,\n" +
+                "  \"array\": [\n" +
+                "    \"2\",\n" +
+                "    \"zwei\"\n" +
+                "  ],\n" +
+                "  \"object\": {\n" +
+                "    \"foo\": null,\n" +
+                "    \"bar\": 7,\n" +
+                "    \"baz\": \"hello world\"\n" +
+                "  }\n" +
+                "}");
+        expect()
+            .body("one", equalTo(1))
+            .root("object")
+                .body("foo", nullValue())
+                .body("bar", equalTo(7))
+                .body("baz", equalTo("hello world"))
+            .root("")
+            .body("array", allOf(hasItem("zwei"), hasItem("2")))
+        .when()
+            .get("health");
+    }
+
+    private void stubHealthJsonToReturn(String body) {
+        server.stubFor(get(urlEqualTo("/health"))
+                .willReturn(aResponse().withHeader("content-type", "application/json")
+                        .withStatus(200)
+                        .withBody(body)
+                )
+        );
     }
 }
