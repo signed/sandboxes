5e131a3f2570af5454ac8068ee53f3480f5efe9f
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2017-08-29 22:26:59 +0200

    add a positive example

--- java/vavr/src/test/java/com/github/signed/sanbox/vavr/SamplesTest.java
+++ java/vavr/src/test/java/com/github/signed/sanbox/vavr/SamplesTest.java
@@ -4,15 +4,19 @@ import io.vavr.control.Try;
 import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class SamplesTest {
 
     @Test
     public void if_an_exception_occurs_empty_is_returned() throws Exception {
-        Try<Integer> divide = divide(1, 0);
-        assertThat("there was an exception, should be empty", divide.isEmpty());
+        assertThat("there was an exception, should be empty", divide(1, 0).isEmpty());
+    }
 
+    @Test
+    public void if_computation_succeeds_the_value_is_returned() throws Exception {
+        assertThat(divide(1,1).get(), equalTo(1));
     }
 
     Try<Integer> divide(int one, int two) {
