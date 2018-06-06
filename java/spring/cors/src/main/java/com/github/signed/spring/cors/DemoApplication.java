3fc1089f8e0652fa2aaa36c34fe550424b1c20fb
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-06-06 23:47:18 +0200

    small cors example

--- java/spring/cors/src/main/java/com/github/signed/spring/cors/DemoApplication.java
+++ java/spring/cors/src/main/java/com/github/signed/spring/cors/DemoApplication.java
@@ -2,11 +2,30 @@ package com.github.signed.spring.cors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    //https://spring.io/guides/gs/rest-service-cors/
+    //https://docs.spring.io/spring/docs/current/spring-framework-reference/web.html#mvc-cors
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/greet")
+    @CrossOrigin()
+    public String get() {
+        return "{}";
+    }
+
+    @PostMapping("/greet")
+    @CrossOrigin
+    public String post() {
+        return "{}";
+    }
 }
