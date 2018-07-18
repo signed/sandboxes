56bb37d901a3823ee22cd2f892be5500c4bc2e90
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-07-18 20:41:06 +0200

    add another module to test some zalando libraries

---
+++ java/spring/spring-boot-extension/zalando/src/main/java/sample/BootApplication.java
@@ -0,0 +1,42 @@
+package sample;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@SpringBootApplication
+public class BootApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(BootApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
+
+    @RequestMapping(method = POST, path = "/")
+    @ResponseBody
+    public ResponseText post(@RequestBody RequestText requestText) {
+        logger.info("action=enter");
+        ResponseText responseText = new ResponseText();
+        responseText.responseText = "processed(" + requestText.requestText + ")";
+        logger.info("action=exit");
+        return responseText;
+    }
+
+    public static class RequestText {
+        public String requestText;
+    }
+
+    public static class ResponseText {
+        public String responseText;
+    }
+
+
+
+}
