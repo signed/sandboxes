f930ee18bfcf6914baae154449e542d631bdaa39
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-07-19 23:24:41 +0200

    generate all files in the dist folder

--- javascript/react-redux/webpack.config.js
+++ javascript/react-redux/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: "development",
@@ -7,6 +8,14 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Getting Started',
+      template: 'public/index.html'
+    })
+  ],
+
   module: {
     rules: [
       {test: /\.txt$/, use: 'raw-loader'},
