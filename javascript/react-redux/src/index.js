4bf93d98b41e48bc7ac6767b99d2049b5a0fc537
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-07-19 22:37:39 +0200

    add timestamp to see if page refeshes

--- javascript/react-redux/src/index.js
+++ javascript/react-redux/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 
 class Welcome extends React.Component {
   render() {
-    return <h1>Hello, {this.props.name}</h1>;
+    return <h1>Hello, {this.props.name} </h1>;
   }
 }
 
 function App() {
+  let date = new Date();
   return (
     <div>
+      <div>{""+date}</div>
       <Welcome name="Sara"/>
       <Welcome name="Cahal"/>
       <Welcome name="Edite"/>
