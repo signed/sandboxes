f01608701df2eaa73df03d542c9e80efd5cba5ff
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-07-21 22:32:55 +0200

    add the clock sample from the react documentation

--- javascript/react-redux/src/index.js
+++ javascript/react-redux/src/index.js
@@ -7,11 +7,44 @@ class Welcome extends React.Component {
   }
 }
 
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
 function App() {
   let date = new Date();
   return (
     <div>
-      <div>{""+date}</div>
+      <Clock/>
       <Welcome name="Sara"/>
       <Welcome name="Cahal"/>
       <Welcome name="Edite"/>
