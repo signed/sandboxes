f0eafc200bccd8f9bf657de0834048f09fb75703
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-07-22 11:57:43 +0200

    export clock into its own file

--- javascript/react-redux/src/index.js
+++ javascript/react-redux/src/index.js
@@ -1,47 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Clock from './clock';
 
 class Welcome extends React.Component {
   render() {
-    return <h1>Hello, {this.props.name} </h1>;
-  }
-}
-
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
+    return <h1>Hello, {this.props.name}</h1>;
   }
 }
 
 function App() {
-  let date = new Date();
   return (
     <div>
       <Clock/>
