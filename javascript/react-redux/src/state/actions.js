2f609fb1fa661dd1e549bd2c9d95f2281a67e506
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-07-26 22:54:15 +0200

    make sure the passed argument is an array

--- javascript/react-redux/src/state/actions.js
+++ javascript/react-redux/src/state/actions.js
@@ -1,6 +1,9 @@
 import * as actionTypes from './actionTypes'
 
 export function replaceItemsWith(items) {
+  if(!Array.isArray(items)){
+    throw 'items argument is not an array';
+  }
   return {
     type: actionTypes.REPLACE_ITEMS,
     payload: {
