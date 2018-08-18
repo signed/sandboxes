48029c9bfb14197163d98804e816450605850a10
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-08-18 23:08:50 +0200

    upgrade to latest webpack

--- javascript/d3/webpack.config.js
+++ javascript/d3/webpack.config.js
@@ -1,22 +1,37 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const paths = {
+    src: path.join(__dirname, ''),
+    dist: path.join(__dirname, 'dist'),
+    data: path.join(__dirname, 'data')
+};
 
 const config = {
+    mode: 'development',
+    context: paths.src,
     entry: {
         main: './app/index.js',
         timescales: './app/timescale/timescales.js',
         negativebar: './app/negativebar/negativebar.js'
     },
 
-    module: {
-        loaders: [
-            {test: /\.tsv$/, loader: "file-loader"},
-        ],
-    },
-
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsv$/,
+                exclude: [/node_modules/],
+                use: [{
+                    loader: "file-loader"
+                }]
+            }
+
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
