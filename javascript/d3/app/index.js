c8f03d6bf902587466c750b4ec108bcd7b7f9eed
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-08-18 22:32:27 +0200

    get rid of lodash

--- javascript/d3/app/index.js
+++ javascript/d3/app/index.js
@@ -1,8 +1,6 @@
-import _ from 'lodash';
-
 function component() {
-    var element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack', 'my', 'pleasure'], ' ');
+    const element = document.createElement('div');
+    element.innerHTML = ['Hello', 'webpack', 'my', 'pleasure'].join(' ');
     return element;
 }
 
