ee00974522049068b72c0144fbcb2f58f3bd4a2c
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-08-20 22:29:28 +0200

    play with the sample
    root nodes are brown, leaf nodes are green

--- javascript/d3/app/dependencytree/dependencytree.js
+++ javascript/d3/app/dependencytree/dependencytree.js
@@ -10,11 +10,11 @@ var treeData =
             {
                 "name": "Level 2: A",
                 "children": [
-                    { "name": "Son of A" },
-                    { "name": "Daughter of A" }
+                    {"name": "Son of A"},
+                    {"name": "Daughter of A"}
                 ]
             },
-            { "name": "Level 2: B" }
+            {"name": "Level 2: B"}
         ]
     };
 // set the dimensions and margins of the diagram
@@ -25,7 +25,12 @@ var margin = {top: 40, right: 90, bottom: 50, left: 90},
 var treemap = d3.tree()
     .size([width, height]);
 //  assigns the data to a hierarchy using parent-child relationships
-var nodes = d3.hierarchy(treeData);
+var nodes = d3.hierarchy(treeData, function (d) {
+    if (!d.children) {
+        return d.children;
+    }
+    return [...d.children, {name: 'surprise'}];
+});
 // maps the node data to the tree layout
 nodes = treemap(nodes);
 // append the svg obgect to the body of the page
@@ -39,30 +44,43 @@ var svg = d3.select("body").append("svg")
             "translate(" + margin.left + "," + margin.top + ")");
 // adds the links between the nodes
 var link = g.selectAll(".link")
-    .data( nodes.descendants().slice(1))
+    .data(nodes.descendants().slice(1))
     .enter().append("path")
     .attr("class", "link")
-    .attr("d", function(d) {
+    .attr("d", function (d) {
         return "M" + d.x + "," + d.y
             + "C" + d.x + "," + (d.y + d.parent.y) / 2
-            + " " + d.parent.x + "," +  (d.y + d.parent.y) / 2
+            + " " + d.parent.x + "," + (d.y + d.parent.y) / 2
             + " " + d.parent.x + "," + d.parent.y;
     });
 // adds each node as a group
 var node = g.selectAll(".node")
     .data(nodes.descendants())
     .enter().append("g")
-    .attr("class", function(d) {
-        return "node" +
-            (d.children ? " node--internal" : " node--leaf"); })
-    .attr("transform", function(d) {
-        return "translate(" + d.x + "," + d.y + ")"; });
+    .attr("class", function (d) {
+        let type;
+        if (d.depth === 0) {
+            type = 'root';
+        } else {
+            type = (d.children ? "trunk" : "leaf");
+        }
+
+        return "node" + ' node--' + type;
+
+    })
+    .attr("transform", function (d) {
+        return "translate(" + d.x + "," + d.y + ")";
+    });
 // adds the circle to the node
 node.append("circle")
     .attr("r", 10);
 // adds the text to the node
 node.append("text")
     .attr("dy", ".35em")
-    .attr("y", function(d) { return d.children ? -20 : 20; })
+    .attr("y", function (d) {
+        return d.children ? -20 : 20;
+    })
     .style("text-anchor", "middle")
-    .text(function(d) { return d.data.name; });
+    .text(function (d) {
+        return d.data.name;
+    });
