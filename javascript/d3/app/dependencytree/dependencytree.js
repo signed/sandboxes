b1a5dfd3bd957dd31179ec37637f6df5ad0f194b
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-08-28 20:11:17 +0200

    sprinkle some const

--- javascript/d3/app/dependencytree/dependencytree.js
+++ javascript/d3/app/dependencytree/dependencytree.js
@@ -3,7 +3,7 @@
 import css from 'style-loader!css-loader!./dependencytree.css';
 import * as d3 from 'd3';
 
-var treeData =
+const treeData =
     {
         "name": "Top Level",
         "children": [
@@ -18,33 +18,33 @@ var treeData =
         ]
     };
 // set the dimensions and margins of the diagram
-var margin = {top: 40, right: 90, bottom: 50, left: 90},
+const margin = {top: 40, right: 90, bottom: 50, left: 90},
     width = 660 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 // declares a tree layout and assigns the size
-var treemap = d3.tree()
+const treemap = d3.tree()
     .size([width, height]);
 //  assigns the data to a hierarchy using parent-child relationships
-var nodes = d3.hierarchy(treeData, function (d) {
+const nodes = d3.hierarchy(treeData, function (d) {
     if (!d.children) {
         return d.children;
     }
     return [...d.children, {name: 'surprise'}];
 });
 // maps the node data to the tree layout
-nodes = treemap(nodes);
+const tree = treemap(nodes);
 // append the svg obgect to the body of the page
 // appends a 'group' element to 'svg'
 // moves the 'group' element to the top left margin
-var svg = d3.select("body").append("svg")
+const svg = d3.select("body").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom),
     g = svg.append("g")
         .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
 // adds the links between the nodes
-var link = g.selectAll(".link")
-    .data(nodes.descendants().slice(1))
+const link = g.selectAll(".link")
+    .data(tree.descendants().slice(1))
     .enter().append("path")
     .attr("class", "link")
     .attr("d", function (d) {
@@ -54,8 +54,8 @@ var link = g.selectAll(".link")
             + " " + d.parent.x + "," + d.parent.y;
     });
 // adds each node as a group
-var node = g.selectAll(".node")
-    .data(nodes.descendants())
+const node = g.selectAll(".node")
+    .data(tree.descendants())
     .enter().append("g")
     .attr("class", function (d) {
         let type;
