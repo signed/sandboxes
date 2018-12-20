b3069e83fa7fad88fb53580006139f2eef9890fe
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-12-20 02:15:34 +0100

    this should work

--- java/javaparser/src/test/java/sample/TestMethodMigration.java
+++ java/javaparser/src/test/java/sample/TestMethodMigration.java
@@ -1,13 +1,19 @@
 package sample;
 
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.ImportDeclaration;
-import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.AnnotationExpr;
+import com.github.javaparser.ast.expr.ClassExpr;
+import com.github.javaparser.ast.expr.MemberValuePair;
+import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.ast.visitor.ModifierVisitor;
 import com.github.javaparser.ast.visitor.Visitable;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import java.util.Optional;
 
@@ -20,9 +26,45 @@ public class TestMethodMigration extends ModifierVisitor<Void> {
     }
 
     @Override
-    public Visitable visit(MethodDeclaration n, Void arg) {
-        Optional<AnnotationExpr> annotationByClass = n.getAnnotationByClass(Test.class);
-        annotationByClass.ifPresent(it -> n.setModifier(Modifier.PUBLIC, false));
-        return n;
+    public Visitable visit(MethodDeclaration methodDeclaration, Void arg) {
+        Optional<AnnotationExpr> annotationByClass = methodDeclaration.getAnnotationByClass(Test.class);
+        annotationByClass.ifPresent(it -> {
+            TestAnnotationMigration.Aggregator aggregator = new TestAnnotationMigration.Aggregator();
+            new TestAnnotationMigration().visit(methodDeclaration.getAnnotations(), aggregator);
+            if( null !=  aggregator.exceptionClass){
+                methodDeclaration.getBody().ifPresent(body -> {
+                    if (body.getStatements().isEmpty()) {
+                        return;
+                    }
+                    String s = body.toString();
+
+                    ImportDeclaration importDeclaration = JavaParser.parseImport("import static " + Assertions.class.getCanonicalName()+ ".assertThrows");
+                    methodDeclaration.findAncestor(CompilationUnit.class).ifPresent(p -> p.addImport(importDeclaration));
+
+                    Statement statement = JavaParser.parseStatement("assertThrows("+aggregator.exceptionClass.toString()+",()->" +
+                            ""+s +
+                            ");\n");
+                    NodeList<Statement> statements = new NodeList<>();
+                    statements.add(statement);
+                    body.setStatements(statements);
+                });
+            }
+        });
+        return methodDeclaration;
+    }
+
+    public static class TestAnnotationMigration extends ModifierVisitor<TestAnnotationMigration.Aggregator>{
+        public static class Aggregator {
+            public ClassExpr exceptionClass;
+        }
+
+        @Override
+        public Visitable visit(MemberValuePair n, Aggregator arg) {
+            if("expected".equals(n.getName().toString())){
+                arg.exceptionClass = (ClassExpr) n.getValue();
+                return null;
+            }
+            throw new RuntimeException("you should handle this one");
+        }
     }
 }
