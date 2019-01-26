e9cbe4035c4a2e2b8bf19f091503627db4bf054c
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-01-27 00:34:30 +0100

    basic react app in typescript with webpacbasic react app in typescript with webpackk

---
+++ typescript/testcafe/src/application.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export default class Application extends React.Component {
+  public render(): React.ReactNode {
+    return (<div>Hello world</div>);
+  }
+}
---
+++ typescript/testcafe/src/index.tsx
@@ -0,0 +1,10 @@
+/* tslint:disable:import-name */
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Application from './application';
+
+const app = (<Application/>);
+ReactDOM.render(
+  app,
+  document.getElementById('root'),
+);
