73bbd3dcb531b81fa96bec2d23752185dfba2d72
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-01-27 16:59:24 +0100

    use the function call style for fixture

--- typescript/testcafe/fixtures/hello-world-fixture.ts
+++ typescript/testcafe/fixtures/hello-world-fixture.ts
@@ -1,7 +1,6 @@
 import { Selector } from 'testcafe';
 
-fixture `Getting Started`
-  .page `http://devexpress.github.io/testcafe/example`;
+fixture('Getting Started').page('http://devexpress.github.io/testcafe/example');
 
 test('My first test', async t => {
   // Test code
