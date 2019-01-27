d96c8378c045659c95e88feefc9ab515c536b5ed
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-01-27 23:47:01 +0100

    get the second element of the list

--- typescript/testcafe/fixtures/hello-world-fixture.ts
+++ typescript/testcafe/fixtures/hello-world-fixture.ts
@@ -1,7 +1,8 @@
 import { Selector } from 'testcafe';
 
-fixture('Getting Started').page('http://devexpress.github.io/testcafe/example');
+fixture('Getting Started').page('http://localhost:8080/');
 
-test('My first test', async (browser) => {
-  // Test code
+test('My first test', async (tc) => {
+  const selector = Selector('#root > ul > li:nth-child(2)');
+  await tc.expect(selector.textContent).eql('two');
 });
