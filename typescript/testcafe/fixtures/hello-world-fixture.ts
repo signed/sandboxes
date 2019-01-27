7dbe1c8d9c17c719d1ed4978c11213f0337509c4
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-01-27 23:56:23 +0100

    use react selectors to access the page

--- typescript/testcafe/fixtures/hello-world-fixture.ts
+++ typescript/testcafe/fixtures/hello-world-fixture.ts
@@ -1,8 +1,18 @@
 import { Selector } from 'testcafe';
+import { ReactSelector, waitForReact } from 'testcafe-react-selectors';
 
-fixture('Getting Started').page('http://localhost:8080/');
+fixture('Getting Started')
+  .page('http://localhost:8080/')
+  .beforeEach(async () => {
+    await waitForReact();
+  });
 
-test('My first test', async (tc) => {
-  const selector = Selector('#root > ul > li:nth-child(2)');
+test('vanilla selectors', async (tc) => {
+  const selector = Selector('#root > ul > li').nth(1);
+  await tc.expect(selector.textContent).eql('two');
+});
+
+test('react selectors', async (tc) => {
+  const selector = ReactSelector('ListItem').nth(1);
   await tc.expect(selector.textContent).eql('two');
 });
