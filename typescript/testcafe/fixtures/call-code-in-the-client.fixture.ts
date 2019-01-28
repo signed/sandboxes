67ec290d29fcd3ea878e921e58a9629aa3280553
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-01-28 22:16:33 +0100

    add another eval example

--- typescript/testcafe/fixtures/call-code-in-the-client.fixture.ts
+++ typescript/testcafe/fixtures/call-code-in-the-client.fixture.ts
@@ -3,6 +3,10 @@ import { Selector } from 'testcafe';
 fixture('Getting Started')
   .page('http://localhost:8080/');
 
-test.only('access document uri in client', async (tc) => {
+test('access document uri in client', async (tc) => {
   await tc.expect(tc.eval(() => document['documentURI'])).eql('http://localhost:8080/') ;
+});
+
+test('Get UA', async (tc) => {
+  await tc.expect(tc.eval(() => navigator.vendor)).eql('Google Inc.');
 });
