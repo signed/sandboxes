bb5c01c2114a30d4f531699906878f153fc7161d
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-01-28 22:27:44 +0100

    sample with a client function

--- typescript/testcafe/fixtures/call-code-in-the-client.fixture.ts
+++ typescript/testcafe/fixtures/call-code-in-the-client.fixture.ts
@@ -1,12 +1,18 @@
-import { Selector } from 'testcafe';
+import { ClientFunction } from 'testcafe';
+
+const getPageUrl = ClientFunction(() => window.location.href);
 
 fixture('Getting Started')
   .page('http://localhost:8080/');
 
 test('access document uri in client', async (tc) => {
-  await tc.expect(tc.eval(() => document['documentURI'])).eql('http://localhost:8080/') ;
+  await tc.expect(tc.eval(() => document['documentURI'])).eql('http://localhost:8080/');
 });
 
 test('Get UA', async (tc) => {
   await tc.expect(tc.eval(() => navigator.vendor)).eql('Google Inc.');
+});
+
+test('Check the page URL', async (tc) => {
+  await tc.expect(getPageUrl()).contains('http://localhost:8080/');
 });
