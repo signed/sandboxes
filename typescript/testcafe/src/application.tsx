a9cee7d35816e54fafa952dbaf1c7bb6931251a8
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-01-30 00:02:36 +0100

    sample with client local storage

--- typescript/testcafe/src/application.tsx
+++ typescript/testcafe/src/application.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import List from './list';
 import RemoteCall from './remote-call';
+import LocalStorageDisplay from './local-storage-display';
 
 export default class Application extends React.Component {
   public render(): React.ReactNode {
@@ -8,6 +9,7 @@ export default class Application extends React.Component {
       <div>Hello World</div>,
       <List/>,
       <RemoteCall/>,
+      <LocalStorageDisplay localStorageKey="key"/>
     ];
   }
 }
