6ee2a3bdee14eb4b42e3c691fd15eadd69448582
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-01-29 21:06:25 +0100

    add sample of a bad test

---
+++ typescript/tests/tests/person.tests.ts
@@ -0,0 +1,15 @@
+import Person, { sortByName } from '../src/person';
+
+describe('sort by name', () => {
+  it('basic example', () => {
+    const one = new Person(1, 'Bob');
+    const two = new Person(2, 'Susan');
+    const three = new Person(3, 'Alice');
+
+    const sorted = sortByName([one, two, three]);
+    console.log(sorted);
+    expect(sorted[0].id).toBe(3);
+    expect(sorted[1].id).toBe(1);
+    expect(sorted[2].id).toBe(2);
+  });
+});
