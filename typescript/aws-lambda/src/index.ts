639b1a14c79c5873351ea2aae90b2a82463908cc
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-02-28 00:29:09 +0100

    link to the event structure

--- typescript/aws-lambda/src/index.ts
+++ typescript/aws-lambda/src/index.ts
@@ -10,6 +10,8 @@ interface Response {
 
 }
 
+// event structure https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/lambda-event-structure.html
+
 export const handler = (event: any = {}, context: any = {}, callback: (error: any, next: Request | Response) => void): void => {
   console.log('Hello World!');
   let data = JSON.stringify(event, null, 2);
