344e7046e41718f5d1c749222ab75c1091c2d093
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-02-28 00:06:06 +0100

    sample with await

--- typescript/aws-lambda/tests/index.spec.ts
+++ typescript/aws-lambda/tests/index.spec.ts
@@ -1,7 +1,12 @@
 import {handler} from "../src";
 
 describe('lambda', () => {
-    it('should run', () => {
+    it('then', () => {
       return handler({}).then(data => expect(data).toEqual('{}'));
+    });
+    it('await', async () => {
+      expect.assertions(1);
+      let data = await handler({});
+      expect(data).toEqual('{ }');
     });
 });
