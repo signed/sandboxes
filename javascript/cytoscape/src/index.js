50b76c1b4265ab91781044d2d6eb90674b78595f
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-03-23 15:42:53 +0100

    reduce duplication during initial data input

--- javascript/cytoscape/src/index.js
+++ javascript/cytoscape/src/index.js
@@ -1,52 +1,47 @@
 import cytoscape from 'cytoscape';
 import dagre from 'cytoscape-dagre';
+
 cytoscape.use(dagre);
 
 
 const options = {
     container: document.getElementById('graph'),
-    elements: [
-        {
-            group: 'nodes',
-            data: {id: 'a'}
-        },
-        {
-            group: 'nodes',
-            data: {id: 'b'}
-        },
-        {
-            group: 'nodes',
-            data: {id: 'b1'}
-        },
-        {
-            group: 'nodes',
-            data: {id: 'b2'}
-        },
-        {
-            group: 'nodes',
-            data: {id: 'c'},
-        },
-        {
-            group: 'nodes',
-            data: {id: 'd'},
-        },
-        {
-            group: 'edges',
-            data: {id: 'ab', source: 'a', target: 'b'}
-        },
-        {
-            group: 'edges',
-            data: {id: 'ab1', source: 'a', target: 'b1'}
-        },
-        {
-            group: 'edges',
-            data: {id: 'ab2', source: 'a', target: 'b2'}
-        },
-        {
-            group: 'edges',
-            data: {id: 'b1c', source: 'b1', target: 'c'}
-        }
-    ],
+    elements: {
+        nodes: [
+            {
+                data: {id: 'a'}
+            },
+            {
+                data: {id: 'b'}
+            },
+            {
+                data: {id: 'b1'}
+            },
+            {
+                data: {id: 'b2'}
+            },
+            {
+                data: {id: 'c'}
+            },
+            {
+                data: {id: 'd'}
+            }
+        ],
+        edges: [
+            {
+                data: {id: 'ab', source: 'a', target: 'b'}
+            },
+            {
+                data: {id: 'ab1', source: 'a', target: 'b1'}
+            },
+            {
+                data: {id: 'ab2', source: 'a', target: 'b2'}
+            },
+            {
+                data: {id: 'b1c', source: 'b1', target: 'c'}
+            }
+        ]
+    },
     style: [ // the stylesheet for the graph
         {
             selector: 'node',
