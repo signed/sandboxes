cac3f3e0d55b0b98b8cfd39feeaa99f30a5b6239
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-04-01 22:49:00 +0200

    more samples

--- typescript/publish-library/usage-javascript/src/index.js
+++ typescript/publish-library/usage-javascript/src/index.js
@@ -4,11 +4,16 @@ function version() {
     return `version: ${library.version}`
 }
 
+function helloPaul() {
+    return library.hello('Paul');
+}
+
 function sum(a, b) {
     return a + b;
 }
 
 module.exports = {
     version,
+    helloPaul,
     sum
 };
--- typescript/publish-library/usage-javascript/tests/index.spec.js
+++ typescript/publish-library/usage-javascript/tests/index.spec.js
@@ -1,11 +1,14 @@
 const index = require('../src/index');
 
 describe('using typescript library', () => {
-    test('adds 1 + 2 to equal 3', () => {
+    it('should work with just the import', () => {
         expect(index.sum(1, 2)).toBe(3);
     });
 
-    it('should ', () => {
+    it('should work with constant', () => {
         expect(index.version()).toEqual('version: 1.0.0');
+    });
+    it('should work with functions', () => {
+        expect(index.helloPaul()).toEqual('Hello Paul');
     });
 });
