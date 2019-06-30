f2bc090eaded6306ed4ba34f22f4ebc06f7b55f1
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-06-30 19:33:32 +0200

    extend example to showcase passing data between hooks and test

--- typescript/testcafe/fixtures/test-hooks.fixture.ts
+++ typescript/testcafe/fixtures/test-hooks.fixture.ts
@@ -3,21 +3,23 @@ import 'testcafe';
 fixture.only('the fixture')
   .page`http://example.com`
   .beforeEach(async t => {
-    console.log('before each');
+    t.ctx.someProp = 'initialize shared prop';
+    console.log(`before each "${t.ctx.someProp}"`);
   })
   .afterEach(async t => {
-    console.log('after each');
+    console.log(`after each "${t.ctx.someProp}"`);
   });
 
 test('asdf', async t => {
-  console.log('the test');
+  console.log(`the test "${t.ctx.someProp}"`);
 });
 
 test
   .before(async t => {
-    console.log('before');
+    console.log(`before "${t.ctx.someProp}"`);
   })('override test hooks', async t => {
-    console.log('test with hook overrides');
-  }).after(async t => {
-    console.log('after');
+    console.log(`test with hook overrides "${t.ctx.someProp}"`);
+  })
+  .after(async t => {
+    console.log(`after "${t.ctx.someProp}"`);
   });
