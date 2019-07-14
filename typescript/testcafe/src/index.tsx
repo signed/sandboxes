fffbfdbbf091b99248d683d8ac6220a9d3979ede
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-07-14 09:49:11 +0200

    testcafe is in its own repo now

--- typescript/testcafe/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Application from './application';
-
-const app = (<Application/>);
-ReactDOM.render(
-  app,
-  document.getElementById('root'),
-);
--- typescript/testcafe/src/list-item.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export default class ListItem extends React.Component<{ content: string; }> {
-  public render() {
-    return (<li>{this.props.content}</li>);
-  }
-}
