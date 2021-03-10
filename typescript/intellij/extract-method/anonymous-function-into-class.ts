219d500255b4947baf8f7ec1276d1ec683822fa1
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-03-10 22:52:41 +0100

    no reproducer yet

---
+++ typescript/intellij/extract-method/anonymous-function-into-class.ts
@@ -0,0 +1,14 @@
+export class SomeClass {
+    aMethod(condition:boolean) {
+        const emitter = new EventSource('stand in');
+        emitter.addEventListener('on', ()=> {
+            this.asdfasd(condition);
+        })
+    }
+
+    protected asdfasd(condition: boolean) {
+        if (condition) {
+            console.log('jup');
+        }
+    }
+}
