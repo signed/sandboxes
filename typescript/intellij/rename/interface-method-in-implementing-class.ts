11ae1f1005cfd28a9d0e92f6e42105fe94e69463
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-04-01 20:43:34 +0200

    reproducer for interface method rename with private field with same name in implementation

--- typescript/intellij/rename/interface-method-in-implementing-class.ts
+++
@@ -1,9 +0,0 @@
-// https://youtrack.jetbrains.com/issue/WEB-50263
-interface AnInterface {
-    methodToRename():void
-}
-
-class AnImplementation implements AnInterface{
-    methodToRename(): void {
-    }
-}
---
+++ typescript/intellij/rename/interface-method-with-same-name-private-field-in-implementing-class.ts
@@ -0,0 +1,15 @@
+// https://youtrack.jetbrains.com/issue/IDEA-265722
+interface AnInterface {
+    newName():void
+}
+
+class AnImplementation implements AnInterface{
+    readonly #toRename: string = 'initial'
+
+    public retrieve() {
+        return this.#toRename
+    }
+
+    newName(): void {
+    }
+}
