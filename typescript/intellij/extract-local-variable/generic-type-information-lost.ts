b62e503a8ae0dc2dcb6bdc3e2aa4a04190f8ce2a
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-04-01 19:52:14 +0200

    add link to ticket

---
+++ typescript/intellij/extract-local-variable/generic-type-information-lost.ts
@@ -0,0 +1,13 @@
+// https://youtrack.jetbrains.com/issue/IDEA-265719
+export class WithGenericMethod<T>{
+    public forEach(callback: (value: T) => void): void {
+    }
+}
+
+export class WithMethod<T> {
+    public method(cache: WithGenericMethod<T>) {
+    }
+}
+
+
+new WithMethod<string>().method(new WithGenericMethod())
