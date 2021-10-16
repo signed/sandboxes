3f3a86ed31e0938eed9fc4299e8e17beea70ec0a
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-10-16 13:29:11 +0200

    polish parser and round trip test

---
+++ json-schema/src/configuration/parser.test.ts
@@ -0,0 +1,83 @@
+import { assertType, Maybe } from '../asserts'
+import { AutoSave, Language, Mode, Theme } from '../generated/settings'
+import { SettingsDto } from './dto'
+import { extractSettings } from './parser'
+
+test('extract known settings and ignore unknown settings', () => {
+  const backendGenerated: SettingsDto = {
+    editor: {
+      'auto-save': {
+        value: false,
+        interval: 45,
+      },
+      unknown: 'not know should be ignored',
+    },
+    ui: {
+      mode: 'dark',
+      unknown: 'not know should be ignored',
+    },
+    unknown: {
+      'category': {
+        unknown: 'not know should be ignored',
+      },
+    },
+  }
+  const receivedJson = sendOverTheWire(backendGenerated)
+  const useAllSettings = ['editor.auto-save', 'general.language', 'ui.mode', 'ui.theme'] as const
+  const settingsDocument = extractSettings(receivedJson, useAllSettings)
+
+  expect(assertType<Maybe<AutoSave>>(settingsDocument['editor.auto-save'])).toStrictEqual({
+    type: 'editor.auto-save',
+    value: {
+      value: false,
+      interval: 45,
+    },
+  })
+  expect(assertType<Maybe<Mode>>(settingsDocument['ui.mode'])).toStrictEqual({
+    type: 'ui.mode',
+    value: 'dark',
+  })
+  expect(Object.keys(settingsDocument)).toStrictEqual(['editor.auto-save', 'ui.mode'])
+  assertType<Maybe<Theme>>(settingsDocument['ui.theme'])
+  assertType<Maybe<Language>>(settingsDocument['general.language'])
+})
+
+test('not all settings', () => {
+  const backendGenerated: SettingsDto = {
+    editor: {
+      'auto-save': {
+        value: false,
+        interval: 45,
+      },
+      unknown: 'not know should be ignored',
+    },
+    ui: {
+      mode: 'dark',
+      unknown: 'not know should be ignored',
+    },
+    unknown: {
+      'category': {
+        unknown: 'not know should be ignored',
+      },
+    },
+  }
+  const receivedJson = sendOverTheWire(backendGenerated)
+  const useSubsetOfSettings = ['ui.mode', 'ui.theme'] as const
+  const settingsDocument = extractSettings(receivedJson, useSubsetOfSettings)
+
+  expect(assertType<Maybe<Mode>>(settingsDocument['ui.mode'])).toStrictEqual({
+    type: 'ui.mode',
+    value: 'dark',
+  })
+  expect(assertType<Maybe<Theme>>(settingsDocument['ui.theme'])).toBe(undefined)
+  expect(Object.keys(settingsDocument)).toStrictEqual(['ui.mode'])
+})
+
+const sendOverTheWire = (backendGenerated: SettingsDto) => {
+  const serialized = JSON.stringify(backendGenerated)
+  return JSON.parse(serialized)
+}
+
+
+
+
--- json-schema/src/round-trip.test.ts
+++ json-schema/src/round-trip.test.ts
@@ -1,61 +1,79 @@
 import { assertType, Maybe } from './asserts'
-import { AutoSave, Language, Mode, Theme } from './generated/settings'
+import { usedSettings as settingsUsedInClientOne } from './configuration/client-one'
+import { settingFor } from './configuration/configuration'
 import { SettingsDto } from './configuration/dto'
-import { usedSettings } from './configuration/client-one'
 import { extractSettings } from './configuration/parser'
+import { Language, SupportedLanguage, SupportedMode, SupportedTheme, Theme } from './generated/settings'
 
-test('extract known settings and ignore unknown settings', () => {
-  const receivedJson = roundTripJson()
-  const settingsDocument = extractSettings(receivedJson, usedSettings)
-
-  expect(assertType<Maybe<AutoSave>>(settingsDocument['editor.auto-save'])).toStrictEqual({
-    type: 'editor.auto-save',
-    value: {
+const backendGenerated: SettingsDto = {
+  editor: {
+    'auto-save': {
       value: false,
       interval: 45,
     },
-  })
-  expect(assertType<Maybe<Mode>>(settingsDocument['ui.mode'])).toStrictEqual({
-    type: 'ui.mode',
-    value: 'dark',
-  })
-  expect(Object.keys(settingsDocument)).toStrictEqual(['editor.auto-save', 'ui.mode'])
+    unknown: 'not know should be ignored',
+  },
+  ui: {
+    mode: 'dark',
+    unknown: 'not know should be ignored',
+  },
+  unknown: {
+    'category': {
+      unknown: 'not know should be ignored',
+    },
+  },
+}
+
+test('extract known settings and ignore unknown settings', () => {
+  const receivedJson = roundTripJson()
+  const settingsDocument = extractSettings(receivedJson, settingsUsedInClientOne)
+
   assertType<Maybe<Theme>>(settingsDocument['ui.theme'])
   assertType<Maybe<Language>>(settingsDocument['general.language'])
+
+  const autoSave = settingFor(settingsDocument, 'editor.auto-save')
+  assertType<Maybe<{
+    value: boolean;
+    interval: number;
+  }>>(autoSave)
+  expect(autoSave).toStrictEqual({
+    value: false,
+    interval: 45,
+  })
+  const language = settingFor(settingsDocument, 'general.language')
+  assertType<SupportedLanguage>(language)
+  expect(language).toBe('EN')
+
+  const mode = settingFor(settingsDocument, 'ui.mode')
+  assertType<SupportedMode>(mode)
+  expect(mode).toBe('dark')
+
+  const theme = settingFor(settingsDocument, 'ui.theme')
+  assertType<Maybe<SupportedTheme>>(theme)
+  expect(theme).toBe(undefined)
 })
 
-export const typesInTwo = ['ui.mode', 'ui.theme'] as const
+export const settingsUsedInClientTwo = ['ui.mode', 'ui.theme'] as const
+
 test('not all settings', () => {
   const receivedJson = roundTripJson()
-  const settingsDocument = extractSettings(receivedJson, typesInTwo)
+  const settingsDocument = extractSettings(receivedJson, settingsUsedInClientTwo)
 
-  expect(assertType<Maybe<Mode>>(settingsDocument['ui.mode'])).toStrictEqual({
-    type: 'ui.mode',
-    value: 'dark',
-  })
-  expect(assertType<Maybe<Theme>>(settingsDocument['ui.theme'])).toBe(undefined)
-  expect(Object.keys(settingsDocument)).toStrictEqual(['ui.mode'])
+  const mode = settingFor(settingsDocument, 'ui.mode')
+  assertType<SupportedMode>(mode)
+  expect(mode).toBe('dark')
+
+  const theme = settingFor(settingsDocument, 'ui.theme')
+  assertType<Maybe<SupportedTheme>>(theme)
+  expect(theme).toBe(undefined)
+
+  // @ts-expect-error types prohibite access to
+  settingFor(settingsDocument, 'general.language')
+  // @ts-expect-error types prohibite access to
+  settingFor(settingsDocument, 'editor.auto-save')
 })
 
 const roundTripJson = () => {
-  const backendGenerated: SettingsDto = {
-    editor: {
-      'auto-save': {
-        value: false,
-        interval: 45,
-      },
-      unknown: 'not know should be ignored',
-    },
-    ui: {
-      mode: 'dark',
-      unknown: 'not know should be ignored',
-    },
-    unknown: {
-      'category': {
-        unknown: 'not know should be ignored',
-      },
-    },
-  }
   const serialized = JSON.stringify(backendGenerated)
   const received = sendOverTheWire(serialized)
   return JSON.parse(received)
