fbe2ddcb7cecee6810bdb1d9877d60d3c4e138f5
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-10-16 11:06:43 +0200

    use ned implementation in tests

--- json-schema/src/configuration/configuration.test.ts
+++ json-schema/src/configuration/configuration.test.ts
@@ -1,43 +1,50 @@
 import { assertType, Maybe } from '../asserts'
 import { SupportedLanguage, SupportedMode, SupportedTheme } from '../generated/settings'
-import { settingFor, settingFor2 } from './configuration'
+import { settingFor } from './configuration'
 import { SettingsDocument } from './parser'
 
+type UsedSettings = 'general.language'| "ui.theme" | "ui.mode" | "editor.auto-save"
+
 test('return the value from the settings', () => {
-  const language = settingFor([{ type: 'general.language', value: 'ES' }], 'general.language')
+  const document: SettingsDocument<UsedSettings> = {}
+  document['general.language'] = { type: 'general.language', value: 'ES' }
+
+  const language = settingFor(document, 'general.language')
   expect(language).toBe('ES')
+  assertType<SupportedLanguage>(language)
 })
 
 test('return default if it exists and not explicit setting', () => {
-  const language = settingFor([], 'general.language')
+  const document: SettingsDocument<UsedSettings> = {}
+
+  const language = settingFor(document, 'general.language')
   expect(language).toBe('EN')
 })
 
 test('values without a default can be undefined', () => {
-  const setting = settingFor([
-    { type: 'editor.auto-save', value: { value: true, interval: 500 } },
-  ], 'editor.auto-save')
-  // @ts-expect-error type should communicate that the setting can be undefined
-  setting.value
-  expect(setting?.value).toEqual(true)
-  expect(setting?.interval).toEqual(500)
+  const document: SettingsDocument<UsedSettings> = {}
+  document['editor.auto-save'] = { type: 'editor.auto-save', value: { value: true, interval: 500 } }
+  const autoSave = settingFor(document, 'editor.auto-save')
+  assertType<Maybe<{
+    value: boolean;
+    interval: number;
+  }>>(autoSave)
+
+  expect(autoSave?.value).toEqual(true)
+  expect(autoSave?.interval).toEqual(500)
 })
 
 test('values without a default can be undefined', () => {
-  const setting = settingFor([], 'editor.auto-save')
+  const document: SettingsDocument<UsedSettings> = {}
+  const setting = settingFor(document, 'editor.auto-save')
   expect(setting).toBe(undefined)
 })
 
 test('quick and dirty', () => {
-  const settings: SettingsDocument<'general.language'| "ui.theme" | "ui.mode" | "editor.auto-save"> = {}
+  const settings: SettingsDocument<UsedSettings> = {}
 
-  assertType<SupportedLanguage>(settingFor2(settings, 'general.language'))
-  assertType<Maybe<SupportedTheme>>(settingFor2(settings, "ui.theme"))
-  const mode = settingFor2(settings, "ui.mode")
+  const theme = settingFor(settings, "ui.theme")
+  assertType<Maybe<SupportedTheme>>(theme)
+  const mode = settingFor(settings, "ui.mode")
   assertType<SupportedMode>(mode)
-  const autosave = settingFor2(settings, "editor.auto-save")
-  assertType<Maybe<{
-    value: boolean;
-    interval: number;
-  }>>(autosave)
 })
--- json-schema/src/configuration/configuration.ts
+++ json-schema/src/configuration/configuration.ts
@@ -1,28 +1,15 @@
 import { defaults, Setting, Settings, SettingTypeWithDefault } from '../generated/settings'
 import { SettingsDocument } from './parser'
 
-export function settingFor<T extends SettingTypeWithDefault>(settings: Setting[], id: T): Settings[T]["value"]
-export function settingFor<T extends keyof Settings>(
-  settings: Setting[],
-  id: T,
-): Settings[T]["value"] | undefined
-export function settingFor<T extends keyof Settings>(settings: Setting[], id: T) {
-  const found = settings.find((setting) => setting.type === id)
-  if (found !== undefined) {
-    return found.value
-  }
-  return defaults[id as SettingTypeWithDefault]
-}
-
-type SettingType = keyof Settings
+type SettingType = Setting['type']
 
 // idea: third override with a default
-export function settingFor2<K extends SettingType, T extends Extract<K, SettingTypeWithDefault>>(settings: SettingsDocument<K>, id: T): Settings[T]['value']
-export function settingFor2<K extends SettingType, T extends Extract<K,SettingType>>(
+export function settingFor<K extends SettingType, T extends Extract<K, SettingTypeWithDefault>>(settings: SettingsDocument<K>, id: T): Settings[T]['value']
+export function settingFor<K extends SettingType, T extends Extract<K,SettingType>>(
   settings: SettingsDocument<K>,
   id: T,
 ): Settings[T]["value"] | undefined
-export function settingFor2<K extends SettingType, T extends K>(settings: SettingsDocument<K>, id: T) {
+export function settingFor<K extends SettingType, T extends K>(settings: SettingsDocument<K>, id: T) {
   const found = settings[id]
   if (found !== undefined) {
     return found.value
