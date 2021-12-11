0e11f2922d274f6b46ba2fac4b3fce12784783d9
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-12-11 19:22:53 +0100

    extend example

--- typescript/intellij/src/extract/local-variable/string-literal.ts
+++ typescript/intellij/src/extract/local-variable/string-literal.ts
@@ -2,8 +2,9 @@ type Options = {
   state: PermissionState
   args: string[]
 }
-
 const objectParameterFunction = (_options: Options) => {}
+
+objectParameterFunction({args:[], state: 'prompt'})
 
 const plainParameterFunction = (_permissionState: PermissionState) => {}
 
