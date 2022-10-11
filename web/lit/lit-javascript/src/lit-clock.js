50e3b3db208910e21413451d47e0ea4e627007db
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2022-10-11 17:00:51 +0200

    clock example

--- web/lit/lit-javascript/src/lit-clock.js
+++ web/lit/lit-javascript/src/lit-clock.js
@@ -2,10 +2,44 @@ import {LitElement, html} from 'lit';
 
 
 class LitClock extends LitElement {
+    static get properties() {
+        return {
+            _now: {state: true}
+        }
+    }
 
+    constructor() {
+        super();
+        this._tick()
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+        this._scheduleTimeUpdate()
+    }
+
+    disconnectedCallback() {
+        globalThis.clearTimeout(this._timeoutIdentifier)
+        this._timeoutIdentifier = undefined
+    }
+
+    _scheduleTimeUpdate () {
+        this._timeoutIdentifier = globalThis.setTimeout(() => {
+            this._tick();
+            this._scheduleTimeUpdate()
+        }, 1000)
+    }
+
+    _tick() {
+        this._now = new Date()
+    }
 
     render() {
-        return html`static clock`
+        return html`
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is ${this._now.toLocaleTimeString()}.</h2>
+            </div>`
     }
 }
 
