15ef269057c795c18b5164e853643d004afa6f66
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2023-01-22 13:17:10 +0100

    recover from corrupt data with preprocess

--- typescript/validation/src/validation.test.ts
+++ typescript/validation/src/validation.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest'
+import { describe, expect, it, test } from 'vitest'
 import { z, ZodError } from 'zod'
 
 describe('string schema', () => {
@@ -35,6 +35,53 @@ describe('string schema', () => {
       if (!parseResult.success) {
         expect(parseResult.error).toBeDefined()
       }
+    })
+  })
+})
+
+describe('cope if expected string array is corrupted', () => {
+  const schema = z.preprocess((data) => {
+    if (Array.isArray(data)) {
+      return data as unknown
+    }
+    // do error reporting
+    return []
+  }, z.array(z.string()))
+  type Cleaned = z.infer<typeof schema>
+  //    ^? type Cleaned = string[]
+
+  test('string', () => {
+    const parsed: Cleaned = schema.parse('corrupted')
+    expect(parsed).toHaveLength(0)
+  })
+  test('object', () => {
+    const parsed: Cleaned = schema.parse({ k: 'corrupted' })
+    expect(parsed).toHaveLength(0)
+  })
+  describe('expected array', () => {
+    test('empty array', () => {
+      const parsed: Cleaned = schema.parse([])
+      expect(parsed).toHaveLength(0)
+    })
+    test('array with string', () => {
+      const parsed: Cleaned = schema.parse(['one'])
+      expect(parsed).toEqual(['one'])
+    })
+    test('array with number', () => {
+      const result = schema.safeParse([42])
+      if (result.success) {
+        expect.fail('parse should fail')
+      }
+      const issues = result.error.issues
+      expect(issues).toHaveLength(1)
+      const issue = issues[0]
+      expect(issue).toStrictEqual({
+        code: 'invalid_type',
+        expected: 'string',
+        received: 'number',
+        path: [0],
+        message: 'Expected string, received number',
+      })
     })
   })
 })
