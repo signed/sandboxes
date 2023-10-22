1095cc00a18e279fe25a5eddf25c0499c20a298e
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2023-10-22 22:46:51 +0200

    update chai types

--- typescript/chai/src/chai-type-augmentation.ts
+++ typescript/chai/src/chai-type-augmentation.ts
@@ -1,8 +1,5 @@
 declare global {
     namespace Chai {
-        interface AssertionStatic extends AssertionPrototype {
-            overwriteProperty(name: string, getter: (this: AssertionStatic, _super: any) => any): void;
-        }
         interface ChaiUtils {
             transferFlags(assertion: AssertionStatic, obj: object, includeAll?: boolean): void;
         }
