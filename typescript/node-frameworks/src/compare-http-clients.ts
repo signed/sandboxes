a57dd92c9a53668755b3fddf355023af1fffd011
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2023-10-25 23:35:59 +0200

    basic express example

--- typescript/node-frameworks/src/compare-http-clients.ts
+++ typescript/node-frameworks/src/compare-http-clients.ts
@@ -1,7 +1,14 @@
 import {compare, Framework, github} from "./compare.js";
 
+// https://github.com/sindresorhus/got#comparison
+// https://www.memberstack.com/blog/node-http-request
 const frameworks: Framework [] = [
   github('axios', 'https://github.com/axios/axios'),
+  github('zodios', 'https://github.com/ecyrbe/zodios'),
+  github('superagent', 'https://github.com/ladjs/superagent'),
+  github('got', 'https://github.com/sindresorhus/got'),
+  github('ky', 'https://github.com/sindresorhus/ky'),
+  github('node-fetch', 'https://github.com/node-fetch/node-fetch'),
 ]
 
 await compare(frameworks)
