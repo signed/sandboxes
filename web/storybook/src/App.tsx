98a7179b417e6722a18f8b14d20979ec9426de63
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-02-10 22:13:58 +0100

    format

--- web/storybook/src/App.tsx
+++ web/storybook/src/App.tsx
@@ -8,7 +8,6 @@ function App() {
 
   return (
     <>
-
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -19,17 +18,12 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   )
 }
