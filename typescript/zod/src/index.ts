d23614f03ca35a1e9047dac88208121e364e06fe
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-02-24 14:41:19 +0100

    zod playground

---
+++ typescript/zod/src/index.ts
@@ -0,0 +1,30 @@
+export function hello(world: string = '🗺️'): string {
+  return `Hello ${world}!`
+}
+
+enum Taste {
+  Sweet = 'sweet',
+  Sour = 'sour',
+}
+
+interface Fruit {
+  taste: Taste
+}
+
+class Apple implements Fruit {
+  public taste: Taste = Taste.Sour
+}
+
+class DuckTyping {
+  public taste: Taste = Taste.Sweet
+}
+
+export function tasteOf(fruit: Fruit): Taste {
+  return fruit.taste
+}
+
+export function basicInterfaces() {
+  console.log(tasteOf(new Apple()))
+  console.log(tasteOf(new DuckTyping()))
+  console.log(tasteOf({ taste: Taste.Sweet }))
+}
