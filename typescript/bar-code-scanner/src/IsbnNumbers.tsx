c071ca8b8dde7b76221fa639d6cc034cf29750ef
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-05-30 16:37:54 +0200

    add download link

--- typescript/bar-code-scanner/src/IsbnNumbers.tsx
+++ typescript/bar-code-scanner/src/IsbnNumbers.tsx
@@ -1,12 +1,35 @@
 import { useIsbnStorage } from './useIsbnStorage.ts'
 
+const downloadData = (filename: string, data: object) => {
+  const blob = new Blob([JSON.stringify(data)], { type: 'text/json' })
+  const link = document.createElement('a')
+
+  link.download = filename
+  link.href = window.URL.createObjectURL(blob)
+  link.dataset.downloadurl = ['text/json', link.download, link.href].join(':')
+  link.click()
+  link.remove()
+}
+const downloadScannedCodes = (data: object) => {
+  downloadData('isbn-numbers.json', data)
+}
+
 export const IsbnNumbers = () => {
-  const state = useIsbnStorage()
+  const { isbnNumbers } = useIsbnStorage()
   return (
     <div>
-      {state.isbnNumbers.length}
+      <button
+        onClick={() => downloadScannedCodes(isbnNumbers)}
+        className="inline-flex items-center rounded bg-gray-300 px-4 py-2 font-bold text-gray-800 hover:bg-gray-400"
+      >
+        <svg className="mr-2 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+          <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+        </svg>
+        <span>Download {isbnNumbers.length}</span>
+      </button>
+
       <ul>
-        {state.isbnNumbers.map((isbn) => (
+        {isbnNumbers.map((isbn) => (
           <li key={isbn}>{isbn}</li>
         ))}
       </ul>
