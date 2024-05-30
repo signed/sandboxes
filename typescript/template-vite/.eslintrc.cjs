493630691e7d5bcff2aaaf75ec849c1c9566f246
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-05-30 17:45:52 +0200

    update eslint configuration

--- typescript/template-vite/.eslintrc.cjs
+++ typescript/template-vite/.eslintrc.cjs
@@ -7,5 +7,28 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/consistent-type-imports': ['error'],
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          {
+            args: 'all',
+            argsIgnorePattern: '^_',
+            caughtErrors: 'all',
+            caughtErrorsIgnorePattern: '^_',
+            destructuredArrayIgnorePattern: '^_',
+            varsIgnorePattern: '^_',
+            ignoreRestSiblings: true,
+          },
+        ],
+        '@typescript-eslint/no-namespace': ['error', { allowDeclarations: true }],
+      },
+    },
+  ],
 }
