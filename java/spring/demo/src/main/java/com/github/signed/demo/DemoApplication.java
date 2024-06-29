a7c205c8e4f14e026e216b34c33ef55f2712d123
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-06-29 17:51:30 +0200

    remove unused imports

--- java/spring/demo/src/main/java/com/github/signed/demo/DemoApplication.java
+++ java/spring/demo/src/main/java/com/github/signed/demo/DemoApplication.java
@@ -2,8 +2,6 @@ package com.github.signed.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
