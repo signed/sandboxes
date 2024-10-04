3716b965d8a39d7ac68dead14cf5a05436c3f4b5
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-10-05 00:16:37 +0200

    still no daemon setup, but a working build

--- docker/build/daemon/build.sh
+++ docker/build/daemon/build.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env sh
 
-docker build -t daemon .
+ImageName=daemon
+
+docker build -t ${ImageName} .
+
+# run the entrypoint with the default command
+echo docker run -it ${ImageName}
+# run the entrypoint and override the command
+echo docker run -it ${ImageName} -a
+# shell into the container
+echo docker run --entrypoint=/bin/sh -it ${ImageName} -c /bin/sh
