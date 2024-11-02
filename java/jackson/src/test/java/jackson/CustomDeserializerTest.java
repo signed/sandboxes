79b68abb8bb706f361d11666eeb8c4eddfac2f86
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-11-02 15:19:27 +0100

    test the other values

--- java/jackson/src/test/java/jackson/CustomDeserializerTest.java
+++ java/jackson/src/test/java/jackson/CustomDeserializerTest.java
@@ -43,26 +43,34 @@ public class CustomDeserializerTest {
     public static class Data {
         @JsonDeserialize(using = CustomDeserializer.class)
         public Migrated value;
+        public int intValue;
+        public String stringValue;
     }
 
     @Test
     void oldRepresentationTrueToEnum() throws JsonProcessingException {
         var oldRepresentation = """
                 {
-                  "value": true
+                  "value": true,
+                  "stringValue": "still working"
                 }
                 """;
-        assertThat(readDataFrom(oldRepresentation).value, is(Migrated.True));
+        final var data = readDataFrom(oldRepresentation);
+        assertThat(data.value, is(Migrated.True));
+        assertThat(data.stringValue, is("still working"));
     }
 
     @Test
     void oldRepresentationFalseToEnum() throws JsonProcessingException {
         var oldRepresentation = """
                 {
-                  "value": false
+                  "value": false,
+                  "intValue": 7
                 }
                 """;
-        assertThat(readDataFrom(oldRepresentation).value, is(Migrated.False));
+        final var data = readDataFrom(oldRepresentation);
+        assertThat(data.value, is(Migrated.False));
+        assertThat(data.intValue, is(7));
     }
 
     @Test
@@ -75,6 +83,8 @@ public class CustomDeserializerTest {
     private Data roundTrip(Migrated value) throws JsonProcessingException {
         final var data = new Data();
         data.value = value;
+        data.intValue = 42;
+        data.stringValue = "Hello World";
 
         final var json = new ObjectMapper().writeValueAsString(data);
         return readDataFrom(json);
