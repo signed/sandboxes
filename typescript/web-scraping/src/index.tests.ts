a7e3cffc71700f4597a5e2a1bb8b8c35c2f58b22
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-11-13 22:03:11 +0100

    web scraping sandbox

---
+++ typescript/web-scraping/src/index.tests.ts
@@ -0,0 +1,10 @@
+import { describe, test, expect } from 'vitest'
+import { hello } from './index.js'
+
+const name: string = 'Susan'
+
+describe('one', () => {
+  test('should fly', () => {
+    expect(hello(name)).toBe('Hello Susan!')
+  })
+})
