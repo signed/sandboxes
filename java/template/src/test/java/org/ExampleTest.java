a2e620dbef3a96f11a3d9f5edb47025842185eb5
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2025-01-30 22:00:50 +0100

    update template

---
+++ java/template/src/test/java/org/ExampleTest.java
@@ -0,0 +1,14 @@
+package org;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ExampleTest {
+    @Test
+    void example() {
+        assertThat(Optional.empty()).isEmpty();
+    }
+}
