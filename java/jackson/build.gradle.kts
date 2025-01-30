d6623ac827d6b029d9cf1de14eeac7b3300f52bb
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2025-01-30 21:26:55 +0100

    update dependencies

--- java/jackson/build.gradle.kts
+++ java/jackson/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `java-library`
+    id("com.github.ben-manes.versions").version("0.52.0")
 }
 
 repositories {
@@ -7,12 +8,12 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("com.fasterxml.jackson:jackson-bom:2.18.1"))
+    implementation(platform("com.fasterxml.jackson:jackson-bom:2.18.2"))
     implementation("com.fasterxml.jackson.core:jackson-core")
     implementation("com.fasterxml.jackson.core:jackson-annotations")
     implementation("com.fasterxml.jackson.core:jackson-databind")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8")
-    testImplementation(platform("org.junit:junit-bom:5.11.3"))
+    testImplementation(platform("org.junit:junit-bom:5.11.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.hamcrest:hamcrest:3.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
