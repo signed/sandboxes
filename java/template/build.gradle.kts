51cb3e7b184c1850855b4749d9c3b6263de2fc12
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2025-01-30 21:40:33 +0100

    java template project

---
+++ java/template/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    `java-library`
+    id("com.github.ben-manes.versions").version("0.52.0")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.11.4"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
