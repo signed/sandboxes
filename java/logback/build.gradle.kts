7a09133d0c094eaf6ad058d7ce8f92da29e50652
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2025-07-15 20:43:03 +0200

    logging dtos

--- java/logback/build.gradle.kts
+++ java/logback/build.gradle.kts
@@ -17,6 +17,10 @@ dependencies {
     testImplementation("org.assertj:assertj-core")
     testImplementation("org.assertj:assertj-guava")
     testImplementation(libs.assertj.vavr)
+    implementation("org.slf4j:slf4j-api:2.0.17")
+    implementation("ch.qos.logback:logback-classic:1.5.18")
+    implementation("ch.qos.logback:logback-core:1.5.18")
+    implementation("net.logstash.logback:logstash-logback-encoder:8.1")
 }
 
 tasks.test {
--- java/logback/src/test/java/org/ExampleTest.java
+++ java/logback/src/test/java/org/ExampleTest.java
@@ -1,6 +1,9 @@
 package org;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Optional;
 
@@ -10,5 +13,23 @@ public class ExampleTest {
     @Test
     void example() {
         assertThat(Optional.empty()).isEmpty();
+    }
+
+    @Test
+    void loggingJsonStrings() {
+        var logger = LoggerFactory.getLogger("Hello Logger");
+        logger.info("\"Hello\"");
+    }
+
+    @Test
+    void loggingDto() {
+        record NestedDto(String chicken) {}
+
+        record LoggingDto(String key, String property, NestedDto nested) {
+        }
+
+        var logger = LoggerFactory.getLogger("Hello Logger");
+        var dto = new LoggingDto("one", "two", new NestedDto("egg"));
+        logger.atInfo().addKeyValue("user_info", dto).log();
     }
 }
