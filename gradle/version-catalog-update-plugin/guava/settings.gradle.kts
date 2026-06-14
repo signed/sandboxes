fb982a92fd59dbe218426784e1eacd84c1426868
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2026-06-14 20:33:37 +0200

    manage plugin repositories in settings.gradle.kts

--- gradle/version-catalog-update-plugin/guava/settings.gradle.kts
+++ gradle/version-catalog-update-plugin/guava/settings.gradle.kts
@@ -1,10 +1,18 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 plugins {
     // Apply the foojay-resolver plugin to allow automatic download of JDKs
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
-    @Suppress("UnstableApiUsage")
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         mavenCentral()
     }
