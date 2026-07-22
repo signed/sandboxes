62cd36aed2f7a8011558a095f0caf456c3a921ba
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2026-07-22 20:22:32 +0200

    setup for stateless4j

---
+++ java/stateless4j/build.gradle.kts
@@ -0,0 +1,35 @@
+import nl.littlerobots.vcu.plugin.resolver.VersionSelectors
+
+plugins {
+    `java-library`
+    alias(libs.plugins.version.catalog.update)
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.stateless4j)
+    implementation(libs.vavr)
+    implementation(libs.jspecific)
+    testImplementation(platform(libs.junit))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation(platform(libs.assertj))
+    testImplementation("org.assertj:assertj-core")
+    testImplementation("org.assertj:assertj-guava")
+    testImplementation(libs.assertj.vavr)
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
+
+versionCatalogUpdate {
+    versionSelector(VersionSelectors.STABLE)
+}
+
---
+++ java/stateless4j/src/main/java/org/example/package-info.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2015-2026 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+/**
+ * Annotation-based conditions for enabling or disabling tests in JUnit Jupiter.
+ */
+@NullMarked
+package org.example;
+
+import org.jspecify.annotations.NullMarked;
